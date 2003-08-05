3a78e7295dc5d07b83539de015b7364da9942605
Jean-Michel Pouré <jm@poure.com>
2003-08-05 12:18:16 +0000

    Updates
    git-svn-id: svn://svn.pgadmin.org/trunk@2007 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_translation_status.php
+++ pgadmin3/cnt/cnt_translation_status.php
@@ -30,6 +30,7 @@
   $translated->addLanguage ("ja_JP", "Japanese", "斉藤　浩 (Hiroshi	Saito)", "saito@inetrt.skcapi.co.jp", "pgadmin3.po", "src/ui/ja_JP/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $translated->addLanguage ("nb_NO", "Norwegian Bokmål", "Brage Førland", "brage@sisyfos.zoo.uib.no", "pgadmin3.po","src/ui/nb_NO/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $translated->addLanguage ("pl_PL", "Polish", "Slawomir Sudnik", "slawek@icpnet.pl", "pgadmin3.po", "src/ui/pl_PL/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
+  $translated->addLanguage ("pt_BR", "Portuguese-Brazilian", "Ângelo Marcos Rigo", "angelo_rigo@yahoo.com.br", "pgadmin3.po","src/ui/pt_BR/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $translated->addLanguage ("ro_RO", "Romanian", "Nagy Karoly", "nagykaroly@expert-software.ro", "pgadmin3.po", "src/ui/ro_RO/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $translated->addLanguage ("ru_RU", "Russian", "Serguei Mokhov", "mokhov@cs.concordia.ca", "pgadmin3.po","src/ui/ru_RU/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $translated->addLanguage ("tr_TR", "Turkish", "Devrim Gunduz",  "devrim@gunduz.org", "pgadmin3.po", "src/ui/tr_TR/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
@@ -40,7 +41,6 @@
   $pending->addLanguage ("cs_CZ", "Czech", "Jan Pruner", "jan@pruner.cz", "pgadmin3.po","src/ui/cs_CZ/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $pending->addLanguage ("es_ES", "Spanish", "Karim Mribti", "karim@inlosa.com", "pgadmin3.po","src/ui/es_ES/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $pending->addLanguage ("is_IS", "Icelandic", "Þórhallur Hálfdánarson", "tolli@tol.li", "pgadmin3.po","src/ui/is_IS/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
-  $pending->addLanguage ("pt_BR", "Portuguese-Brazilian", "Ângelo Marcos Rigo", "angelo_rigo@yahoo.com.br", "pgadmin3.po","src/ui/pt_BR/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $pending->addLanguage ("sd_IN", "Sindhi", "Tariq	Farooqi", "mahin@linux.net.pk", "pgadmin3.po","src/ui/sd_IN/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $pending->addLanguage ("sv_SE", "Swedish", "Dennis	Björklund", "db@zigo.dhs.org", "pgadmin3.po","src/ui/sv_SE/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $pending->addLanguage ("tl_PH", "Filipino/Tagalog", "Howard Bagcat", "howard@mu.edu.ph", "pgadmin3.po","src/ui/tl_PH/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");

13f89238f4fa0f4d9c4f2c63335e5401a7962f5a
Jean-Michel Pouré <jm@poure.com>
2003-08-06 14:12:01 +0000

    Initial upload.
    git-svn-id: svn://svn.pgadmin.org/trunk@2027 a7884b65-44f6-0310-8a51-81a127f17b15

---
+++ pgadmin3/cnt/cnt_mirrors.php
@@ -0,0 +1,74 @@
+<table width="100%"><tr><td colspan=5 align=left><?php echo _("Download pgAdmin from one of PostgreSQL mirrors:") ?> <br><br></td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp.au.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/Australia.gif" border=1 alt="Australia" width="32" height="21"><br>Australia</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.at.postgresql.org/db/www.postgresql.org/pub/pgadmin3"><img src="img/flags/Austria.gif" border=1 alt="Austria" width="32" height="21"><br>Austria</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.be.postgresql.org/postgresql/pgadmin3"><img src="img/flags/Belgium.gif" border=1 alt="Belgium" width="32" height="21"><br>Belgium</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.br.postgresql.org/pub/PostgreSQL/pgadmin3"><img src="img/flags/Brazil.gif" border=1 alt="Brazil" width="32" height="21"><br>Brazil</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp3.ca.postgresql.org/pub/pgadmin3"><img src="img/flags/Canada.gif" border=1 alt="Canada" width="32" height="21"><br>Canada</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp.cl.postgresql.org/pub/GNU/pgsql/pgadmin3"><img src="img/flags/Chile.gif" border=1 alt="Chile" width="32" height="21"><br>Chile</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp2.cr.postgresql.org/pub/Unix/postgres/pgadmin3"><img src="img/flags/CostaRica.gif" border=1 alt="Costa Rica" width="32" height="21"><br>Costa Rica</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.cz.postgresql.org/pub/ftp.postgresql.org/pgadmin3"><img src="img/flags/CzechRepublic.gif" border=1 alt="Czech Republic" width="32" height="21"><br>Czech Republic</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp2.cz.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/CzechRepublic.gif" border=1 alt="Czech Republic" width="32" height="21"><br>Czech Republic</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.dk.postgresql.org/mirrors/postgresql/pgadmin3"><img src="img/flags/Denmark.gif" border=1 alt="Denmark" width="32" height="21"><br>Denmark</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp2.dk.postgresql.org/pub/PostgreSQL/pgadmin3"><img src="img/flags/Denmark.gif" border=1 alt="Denmark" width="32" height="21"><br>Denmark</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.ee.postgresql.org/mirrors/postgresql/pgadmin3"><img src="img/flags/Estonia.gif" border=1 alt="Estonia" width="32" height="21"><br>Estonia</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.fr.postgresql.org/pgadmin3"><img src="img/flags/France.gif" border=1 alt="France" width="32" height="21"><br>France</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp2.fr.postgresql.org/postgresql/pgadmin3"><img src="img/flags/France.gif" border=1 alt="France" width="32" height="21"><br>France</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.de.postgresql.org/mirror/postgresql/pgadmin3"><img src="img/flags/Germany.gif" border=1 alt="Germany" width="32" height="21"><br>Germany</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp2.de.postgresql.org/pub/comp/os/unix/database/postgresql/pgadmin3"><img src="img/flags/Germany.gif" border=1 alt="Germany" width="32" height="21"><br>Germany</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp3.de.postgresql.org/pub/Mirrors/ftp.postgresql.org/pgadmin3"><img src="img/flags/Germany.gif" border=1 alt="Germany" width="32" height="21"><br>Germany</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp4.de.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/Germany.gif" border=1 alt="Germany" width="32" height="21"><br>Germany</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.hk.postgresql.org/postgresql/pgadmin3"><img src="img/flags/HongKong.gif" border=1 alt="Hong Kong" width="32" height="21"><br>Hong Kong</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.hu.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/Hungary.gif" border=1 alt="Hungary" width="32" height="21"><br>Hungary</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp2.is.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/Iceland.gif" border=1 alt="Iceland" width="32" height="21"><br>Iceland</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.ie.postgresql.org/mirrors/ftp.postgresql.org/pub/pgadmin3"><img src="img/flags/Ireland.gif" border=1 alt="Ireland" width="32" height="21"><br>Ireland</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp2.ie.postgresql.org/mirrors/ftp.postgresql.org/pgadmin3"><img src="img/flags/Ireland.gif" border=1 alt="Ireland" width="32" height="21"><br>Ireland</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.il.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/Israel.gif" border=1 alt="Israel" width="32" height="21"><br>Israel</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.it.postgresql.org/mirror/postgresql/pgadmin3"><img src="img/flags/Italy.gif" border=1 alt="Italy" width="32" height="21"><br>Italy</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp2.it.postgresql.org/mirrors/postgres/pgadmin3"><img src="img/flags/Italy.gif" border=1 alt="Italy" width="32" height="21"><br>Italy</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp3.it.postgresql.org/postgresql/pgadmin3"><img src="img/flags/Italy.gif" border=1 alt="Italy" width="32" height="21"><br>Italy</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.jp.postgresql.org/pgadmin3"><img src="img/flags/Japan.gif" border=1 alt="Japan" width="32" height="21"><br>Japan</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp4.jp.postgresql.org/mirror/postgresql/pgadmin3"><img src="img/flags/Japan.gif" border=1 alt="Japan" width="32" height="21"><br>Japan</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.kr.postgresql.org/mirror/database/postgresql/pgadmin3"><img src="img/flags/Korea.gif" border=1 alt="Korea" width="32" height="21"><br>Korea</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp.lv.postgresql.org/pub/software/postgresql/pgadmin3"><img src="img/flags/Latvia.gif" border=1 alt="Latvia" width="32" height="21"><br>Latvia</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.eu.postgresql.org/pub/unix/db/postgresql/pgadmin3"><img src="img/flags/Netherlands.gif" border=1 alt="Netherlands" width="32" height="21"><br>Netherlands</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.nl.postgresql.org:21/pub/mirror/postgresql/pgadmin3"><img src="img/flags/Netherlands.gif" border=1 alt="Netherlands" width="32" height="21"><br>Netherlands</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp2.nl.postgresql.org/mirror/postgresql/pgadmin3"><img src="img/flags/Netherlands.gif" border=1 alt="Netherlands" width="32" height="21"><br>Netherlands</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp4.nl.postgresql.org/postgresql.zeelandnet.nl/pgadmin3"><img src="img/flags/Netherlands.gif" border=1 alt="Netherlands" width="32" height="21"><br>Netherlands</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp.no.postgresql.org/pub/databases/postgresql/pgadmin3"><img src="img/flags/Norway.gif" border=1 alt="Norway" width="32" height="21"><br>Norway</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp2.pl.postgresql.org/mirrors/ftp.postgresql.org/pgadmin3"><img src="img/flags/Poland.gif" border=1 alt="Poland" width="32" height="21"><br>Poland</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp6.pl.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/Poland.gif" border=1 alt="Poland" width="32" height="21"><br>Poland</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp7.pl.postgresql.org/pub/mirror/ftp.postgresql.org/pgadmin3"><img src="img/flags/Poland.gif" border=1 alt="Poland" width="32" height="21"><br>Poland</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.pt.postgresql.org/pgadmin3"><img src="img/flags/Portugal.gif" border=1 alt="Portugal" width="32" height="21"><br>Portugal</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp6.ro.postgresql.org/pub/mirrors/ftp.postgresql.org/pgadmin3"><img src="img/flags/Romania.gif" border=1 alt="Romania" width="32" height="21"><br>Romania</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.ru.postgresql.org/pub/unix/database/pgsql/pgadmin3"><img src="img/flags/Russia.gif" border=1 alt="Russia" width="32" height="21"><br>Russia</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp2.ru.postgresql.org/pgadmin3"><img src="img/flags/Russia.gif" border=1 alt="Russia" width="32" height="21"><br>Russia</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp3.ru.postgresql.org/pub/mirror/postgresql/pub/pgadmin3"><img src="img/flags/Russia.gif" border=1 alt="Russia" width="32" height="21"><br>Russia</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.za.postgresql.org:80/pgadmin3"><img src="img/flags/SouthAfrica.gif" border=1 alt="South Africa" width="32" height="21"><br>South Africa</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp4.es.postgresql.org/Mirror/www.postgresql.org/pgadmin3"><img src="img/flags/Spain.gif" border=1 alt="Spain" width="32" height="21"><br>Spain</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp5.es.postgresql.org/mirror/postgresql/pgadmin3"><img src="img/flags/Spain.gif" border=1 alt="Spain" width="32" height="21"><br>Spain</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.se.postgresql.org/pub/database/relational/postgresql/pgadmin3"><img src="img/flags/Sweden.gif" border=1 alt="Sweden" width="32" height="21"><br>Sweden</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp2.ch.postgresql.org/mirror/postgresql/pgadmin3"><img src="img/flags/Switzerland.gif" border=1 alt="Switzerland" width="32" height="21"><br>Switzerland</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.tw.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/Taiwan.gif" border=1 alt="Taiwan" width="32" height="21"><br>Taiwan</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp3.tw.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/Taiwan.gif" border=1 alt="Taiwan" width="32" height="21"><br>Taiwan</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp5.tw.postgresql.org/pub/Unix/Database/PostgreSQL/pgadmin3"><img src="img/flags/Taiwan.gif" border=1 alt="Taiwan" width="32" height="21"><br>Taiwan</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp.tr.postgresql.org/pub/SQL/postgresql/pgadmin3"><img src="img/flags/Turkey.gif" border=1 alt="Turkey" width="32" height="21"><br>Turkey</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp2.uk.postgresql.org/pgadmin3"><img src="img/flags/UK.gif" border=1 alt="UK" width="32" height="21"><br>UK</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp3.us.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/USA.gif" border=1 alt="USA" width="32" height="21"><br>USA</a><br>&nbsp;</td>
+</tr>
+<tr><td align=center nowrap width="20%"><a href="ftp://ftp5.us.postgresql.org/pub/PostgreSQL/pgadmin3"><img src="img/flags/USA.gif" border=1 alt="USA" width="32" height="21"><br>USA</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp8.us.postgresql.org/pub/pgsql/pgadmin3"><img src="img/flags/USA.gif" border=1 alt="USA" width="32" height="21"><br>USA</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp9.us.postgresql.org/pub/mirrors/postgresql/pgadmin3"><img src="img/flags/USA.gif" border=1 alt="USA" width="32" height="21"><br>USA</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp10.us.postgresql.org/pub/postgresql/pgadmin3"><img src="img/flags/USA.gif" border=1 alt="USA" width="32" height="21"><br>USA</a><br>&nbsp;</td>
+<td align=center nowrap width="20%"><a href="ftp://ftp15.us.postgresql.org/pgadmin3"><img src="img/flags/USA.gif" border=1 alt="USA" width="32" height="21"><br>USA</a><br>&nbsp;</td>
+</table>

14acb1f472464b147bf0865cccaa5bbfff421ac1
Jean-Michel Pouré <jm@poure.com>
2003-08-12 22:07:47 +0000

    More info about CVS.
    git-svn-id: svn://svn.pgadmin.org/trunk@2104 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_development.php
+++ pgadmin3/cnt/cnt_development.php
@@ -14,7 +14,7 @@
   <dd>
   	<?php echo _("pgAdmin source code is available subject to the terms of the Artistic Licence.");?>
     <?php echo sprintf(_("CVS access to the repository is available through a <a href='%s'>web interface</a> or using your favourite CVS client."), "http://cvs.pgadmin.org");?>
-    <?php echo _("Anonymous read access is available at the following CVSRoot without password.");?>
+    <?php echo _("Anonymous read access is available using a CVS client.");?>
     <pre>
     # CVSROOT=:pserver:anonymous@cvs.pgadmin.org:/disk1/cvsroot export CVSROOT
     # cvs login

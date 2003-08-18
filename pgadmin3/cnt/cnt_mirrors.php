e86a1768efe716a43f8f3fc6a52da5976fafd2c9
Jean-Michel Pouré <jm@poure.com>
2003-08-18 21:45:06 +0000

    Simplifications. This script is broken because it should use pg_fetch_all. In case of heavy load, the database will be overfloaded with queries.
    git-svn-id: svn://svn.pgadmin.org/trunk@2147 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_mirrors.php
+++ pgadmin3/cnt/cnt_mirrors.php
@@ -2,46 +2,70 @@
 
   $numcells = 5;
   $conn = @pg_pconnect("dbname=186_portal host=jupiter.hub.org user=186_pgsql");
-
-  // Some seriously nasty nested code...
-
+	$_error = false;
+  
+  //
+  // Create a connexion
+	//
   if(!$conn) {
-    $content = _("<p>Couldn't connect to the database!</p><p>In the meantime, please feel free to download pgAdmin from the primary PostgreSQL FTP site <a href=\"http://www.postgresql.org/ftpsite/\">here</a></p>");
-  } else {
-    $prompt = 'Please select a download mirror: ';
+    echo _("Could not connect to the mirror database.")."<br>";
+    echo sprintf(_("Feel free to download pgAdmin from <a href='%s'>PostgreSQL primary FTP site</a>."), "http://www.postgresql.org/ftpsite")."<br>";
+    $_error = true;
+  }
+
+  //
+  // Execute query
+	//
+
+  if (!$_error) {
+    $prompt = _('Please select a download mirror:');
 
     $query = "SELECT 'ftp' || CASE WHEN mirror_index = 0 THEN ''::text ELSE mirror_index::text END || '.' || country_code || '.postgresql.org' AS hostname, host_port AS port, host_path AS path, country_name AS country FROM mirrors WHERE mirror_type = 'ftp' AND mirror_active = TRUE AND mirror_private = FALSE AND mirror_dns = TRUE AND mirror_last_rsync > (now() - '48 hrs'::interval) ORDER BY country_name, mirror_index;";
     $res = pg_exec($conn,$query);
+
+
     if(!$res) {
-      $content = _("<p>Couldn't query the mirrors table!</p><p>In the meantime, please feel free to download pgAdmin from the primary PostgreSQL FTP site <a href=\"http://www.postgresql.org/ftpsite/\">here</a></p>");
-    } else {
-      $ii = pg_numrows($res);
-      if($ii == 0) {
-        $content = _("<p>No mirrors could be found!</p><p>In the meantime, please feel free to download pgAdmin from the primary PostgreSQL FTP site <a href=\"http://www.postgresql.org/ftpsite/\">here</a></p>");
-      } else {
-        $content = "<table width=\"100%\"><tr><td colspan=$numcells align=left><font size=4>$prompt<br><br></font></td>\n";
-           for($x=0;$x<$ii;$x++) {
-          if(($x%$numcells) == 0) {
-            $content .= "</tr>\n<tr>";
-          }
-          $row = pg_fetch_row($res,$x);
-          $img = str_replace(" ","",$row[3]);
-          $img .= ".gif";
-          if ($row[1] == "") {
-            $content .= "<td align=\"center\" valign=\"top\" width=\"" . 100 / $numcells . "%\"><a href=\"ftp://$row[0]$row[2]\"><img src=\"http://www.postgresql.org/images/flags/$img\" border=1 alt=\"" . ucwords($row[3]) . "\" width=\"32\" height=\"21\"><br>" . ucwords($row[3]) . "</a><br>&nbsp;</td>\n";
-          } else {
-            $content .= "<td align=\"center\" valign=\"top\" width=\"" . 100 / $numcells . "%\"><a href=\"ftp://$row[0]:$row[1]$row[2]\"><img src=\"http://www.postgresql.org/images/flags/$img\" border=1 alt=\"$row[3]\" width=\"32\" height=\"21\"><br>" . ucwords($row[3]) . "</a><br>&nbsp;</td>\n";
-          }
-        }
-        $ii = $numcells - ($x%$numcells);
-        if($ii != $numcells) {
-          $content .= "<td colspan='$ii'>&nbsp;</td>";
-        }
-        $content .= "</table>\n";
-      }
+      echo _("Could not connect to the mirror table.")."<br>";
+      echo sprintf(_("Feel free to download pgAdmin from <a href='%s'>PostgreSQL primary FTP site</a>."), "http://www.postgresql.org/ftpsite")."<br>";
+      $_error = true;
     }
   }
 
-  echo $content;
+//
+// Display resultset
+//
+if (!$_error) {
+	//
+  // Warning: should use pg_fetch_all
+  // to avoid to many queries over the Internet
+  //
+  $ii = pg_numrows($res);
+  if($ii == 0) {
+    echo _("No mirrors could be found.")."<br>";
+    echo sprintf(_("Feel free to download pgAdmin from <a href='%s'>PostgreSQL primary FTP site</a>."), "http://www.postgresql.org/ftpsite")."<br>";
+  } else {
+    $content = "<table width='100%'><tr><td colspan=$numcells align=left><font size=4>$prompt<br><br></font></td>\n";
+
+    for($x=0;$x<$ii;$x++) {
+      if(($x%$numcells) == 0) {
+        $content .= "</tr>\n<tr>";
+      }
+      $row = pg_fetch_row($res,$x);
+      $img = str_replace(" ","",$row[3]);
+      $img .= ".gif";
+      if ($row[1] == "") {
+        $content .= "<td align='center' valign='top' width='" . 100 / $numcells . "%'><a href='ftp://$row[0]$row[2]'><img src='http://www.postgresql.org/images/flags/$img' border=1 alt='" . ucwords($row[3]) . "' width='32' height='21'><br>" . ucwords($row[3]) . "</a><br>&nbsp;</td>\n";
+      } else {
+        $content .= "<td align='center' valign='top' width='" . 100 / $numcells . "%'><a href='ftp://$row[0]:$row[1]$row[2]'><img src='http://www.postgresql.org/images/flags/$img' border=1 alt='$row[3]' width='32' height='21'><br>" . ucwords($row[3]) . "</a><br>&nbsp;</td>\n";
+      }
+    }
+    $ii = $numcells - ($x%$numcells);
+    if($ii != $numcells) {
+      $content .= "<td colspan='$ii'>&nbsp;</td>";
+    }
+    $content .= "</table>\n";
+    echo $content;
+  }
+}
 
 ?>

74cb18f7457bc827a242ced5c0b87a146b3f2bad
Dave Page <dpage@pgadmin.org>
2003-08-18 13:20:01 +0000

    Bracket misplaced.
    git-svn-id: svn://svn.pgadmin.org/trunk@2142 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_mirrors.php
+++ pgadmin3/cnt/cnt_mirrors.php
@@ -9,40 +9,39 @@
     $content = _("<p>Couldn't connect to the database!</p><p>In the meantime, please feel free to download pgAdmin from the primary PostgreSQL FTP site <a href=\"http://www.postgresql.org/ftpsite/\">here</a></p>");
   } else {
     $prompt = 'Please select a download mirror: ';
+
+    $query = "SELECT 'ftp' || CASE WHEN mirror_index = 0 THEN ''::text ELSE mirror_index::text END || '.' || country_code || '.postgresql.org' AS hostname, host_port AS port, host_path AS path, country_name AS country FROM mirrors WHERE mirror_type = 'ftp' AND mirror_active = TRUE AND mirror_private = FALSE AND mirror_dns = TRUE AND mirror_last_rsync > (now() - '48 hrs'::interval) ORDER BY country_name, mirror_index;";
+    $res = pg_exec($conn,$query);
+    if(!$res) {
+      $content = _("<p>Couldn't query the mirrors table!</p><p>In the meantime, please feel free to download pgAdmin from the primary PostgreSQL FTP site <a href=\"http://www.postgresql.org/ftpsite/\">here</a></p>");
+    } else {
+      $ii = pg_numrows($res);
+      if($ii == 0) {
+        $content = _("<p>No mirrors could be found!</p><p>In the meantime, please feel free to download pgAdmin from the primary PostgreSQL FTP site <a href=\"http://www.postgresql.org/ftpsite/\">here</a></p>");
+      } else {
+        $content = "<table width=\"100%\"><tr><td colspan=$numcells align=left><font size=4>$prompt<br><br></font></td>\n";
+           for($x=0;$x<$ii;$x++) {
+          if(($x%$numcells) == 0) {
+            $content .= "</tr>\n<tr>";
+          }
+          $row = pg_fetch_row($res,$x);
+          $img = str_replace(" ","",$row[3]);
+          $img .= ".gif";
+          if ($row[1] == "") {
+            $content .= "<td align=\"center\" valign=\"top\" width=\"" . 100 / $numcells . "%\"><a href=\"ftp://$row[0]$row[2]\"><img src=\"http://www.postgresql.org/images/flags/$img\" border=1 alt=\"" . ucwords($row[3]) . "\" width=\"32\" height=\"21\"><br>" . ucwords($row[3]) . "</a><br>&nbsp;</td>\n";
+          } else {
+            $content .= "<td align=\"center\" valign=\"top\" width=\"" . 100 / $numcells . "%\"><a href=\"ftp://$row[0]:$row[1]$row[2]\"><img src=\"http://www.postgresql.org/images/flags/$img\" border=1 alt=\"$row[3]\" width=\"32\" height=\"21\"><br>" . ucwords($row[3]) . "</a><br>&nbsp;</td>\n";
+          }
+        }
+        $ii = $numcells - ($x%$numcells);
+        if($ii != $numcells) {
+          $content .= "<td colspan='$ii'>&nbsp;</td>";
+        }
+        $content .= "</table>\n";
+      }
+    }
   }
 
-  $query = "SELECT 'ftp' || CASE WHEN mirror_index = 0 THEN ''::text ELSE mirror_index::text END || '.' || country_code || '.postgresql.org' AS hostname, host_port AS port, host_path AS path, country_name AS country FROM mirrors WHERE mirror_type = 'ftp' AND mirror_active = TRUE AND mirror_private = FALSE AND mirror_dns = TRUE AND mirror_last_rsync > (now() - '48 hrs'::interval) ORDER BY country_name, mirror_index;";
-   $res = pg_exec($conn,$query);
-   if(!$res) {
-     $content = _("<p>Couldn't query the mirrors table!</p><p>In the meantime, please feel free to download pgAdmin from the primary PostgreSQL FTP site <a href=\"http://www.postgresql.org/ftpsite/\">here</a></p>");
-   } else {
-     $ii = pg_numrows($res);
-     if($ii == 0) {
-       $content = _("<p>No mirrors could be found!</p><p>In the meantime, please feel free to download pgAdmin from the primary PostgreSQL FTP site <a href=\"http://www.postgresql.org/ftpsite/\">here</a></p>");
-     } else {
-       $content = "<table width=\"100%\"><tr><td colspan=$numcells align=left><font size=4>$prompt<br><br></font></td>\n";
-
-       for($x=0;$x<$ii;$x++) {
-         if(($x%$numcells) == 0) {
-           $content .= "</tr>\n<tr>";
-         }
-         $row = pg_fetch_row($res,$x);
-         $img = str_replace(" ","",$row[3]);
-         $img .= ".gif";
-         if ($row[1] == "") {
-           $content .= "<td align=\"center\" valign=\"top\" width=\"" . 100 / $numcells . "%\"><a href=\"ftp://$row[0]$row[2]\"><img src=\"http://www.postgresql.org/images/flags/$img\" border=1 alt=\"" . ucwords($row[3]) . "\" width=\"32\" height=\"21\"><br>" . ucwords($row[3]) . "</a><br>&nbsp;</td>\n";
-         } else {
-           $content .= "<td align=\"center\" valign=\"top\" width=\"" . 100 / $numcells . "%\"><a href=\"ftp://$row[0]:$row[1]$row[2]\"><img src=\"http://www.postgresql.org/images/flags/$img\" border=1 alt=\"$row[3]\" width=\"32\" height=\"21\"><br>" . ucwords($row[3]) . "</a><br>&nbsp;</td>\n";
-         }
-       }
-       $ii = $numcells - ($x%$numcells);
-       if($ii != $numcells) {
-         $content .= "<td colspan='$ii'>&nbsp;</td>";
-       }
-       $content .= "</table>\n";
-     }
-   }
-
-   echo $content;
+  echo $content;
 
 ?>

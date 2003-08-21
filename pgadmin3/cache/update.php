3ead81d5e2c45a43c419bba9295e74955bde8bec
Jean-Michel Pouré <jm@poure.com>
2003-08-21 09:32:41 +0000

    Because statistics are now stored in CVS, the update.php utility can only be run locally.
    git-svn-id: svn://svn.pgadmin.org/trunk@2182 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cache/update.php
+++ pgadmin3/cache/update.php
@@ -1,22 +1,26 @@
 <?php
-	require_once "../class/pgadmin_po.php";
+	if ($_SERVER["REMOTE_ADDR"]="127.0.0.1") {
+    require_once "../class/pgadmin_po.php";
 
-  $translated = new po_status();
-  $pending = new po_status();
+    $translated = new po_status();
+    $pending = new po_status();
 
-  $translated->setWebRoot($_SERVER["DOCUMENT_ROOT"]);
-  $translated->setCvsRoot($_SERVER["DOCUMENT_ROOT"]."/cvsroot/pgadmin3");
+    $translated->setWebRoot($_SERVER["DOCUMENT_ROOT"]);
+    $translated->setCvsRoot($_SERVER["DOCUMENT_ROOT"]."/cvsroot/pgadmin3");
 
-  $pending->setWebRoot($_SERVER["DOCUMENT_ROOT"]);
-  $pending->setCvsRoot($_SERVER["DOCUMENT_ROOT"]."/cvsroot/pgadmin3");
+    $pending->setWebRoot($_SERVER["DOCUMENT_ROOT"]);
+    $pending->setCvsRoot($_SERVER["DOCUMENT_ROOT"]."/cvsroot/pgadmin3");
 
-  require_once "../cnt/cnt_translation_status.php";
+    require_once "../cnt/cnt_translation_status.php";
 
-  $_cache_translated = dirname($_SERVER["SCRIPT_FILENAME"])."/cache_translated.txt";
-  $_cache_pending    = dirname($_SERVER["SCRIPT_FILENAME"])."/cache_pending.txt";
+    $_cache_translated = dirname($_SERVER["SCRIPT_FILENAME"])."/cache_translated.txt";
+    $_cache_pending    = dirname($_SERVER["SCRIPT_FILENAME"])."/cache_pending.txt";
 
-  $translated->_cache_save($_cache_translated);
-  $pending->_cache_save($_cache_pending);
+    $translated->_cache_save($_cache_translated);
+    $pending->_cache_save($_cache_pending);
 
-  echo "Translation statistics updated.<br>";
+    echo "Translation statistics updated.<br>";
+  } else {
+		Echo "Sorry, this cache update utility can only be run locally.";
+  }
 ?>

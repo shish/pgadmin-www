e7fea1dfb2d069c1d0a8bab9dfc5130b72991ed0
Jean-Michel Pouré <jm@poure.com>
2003-08-28 11:56:10 +0000

    default css for latin1
    git-svn-id: svn://svn.pgadmin.org/trunk@2255 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/tpl/head.php
+++ pgadmin3/tpl/head.php
@@ -1,3 +1,28 @@
+<?php
+  $_css=$_SESSION['pgadmin']['locale'];
+
+  switch ($_css){
+  case "en_US":
+    $_SESSION['pgadmin']['css'] = "bluehaze-latin1.css";
+    break;
+
+  case "de_DE":
+    $_SESSION['pgadmin']['css'] = "bluehaze-latin1.css";
+    break;
+
+  case "fr_FR":
+    $_SESSION['pgadmin']['css'] = "bluehaze-latin1.css";
+    break;
+
+  case "id_ID":
+    $_SESSION['pgadmin']['css'] = "bluehaze-latin1.css";
+    break;
+
+  default:
+    $_SESSION['pgadmin']['css'] = "bluehaze.css";
+  }
+?>
+
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="css/bluehaze.css" title="Blue Haze stylesheet" />
+<link rel="stylesheet" type="text/css" href="css/<?php echo $_SESSION['pgadmin']['css']?>" title="Blue Haze stylesheet" />
 <link rel="stylesheet" type="text/css" href="css/color-scheme.css" title="Blue Haze stylesheet" />

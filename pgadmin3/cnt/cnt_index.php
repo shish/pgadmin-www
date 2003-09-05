2b56e446784b3a8a4857c952efae6f5dd4fffac6
Jean-Michel Pouré <jm@poure.com>
2003-09-05 21:35:47 +0000

    Fix.
    git-svn-id: svn://svn.pgadmin.org/trunk@2373 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_index.php
+++ pgadmin3/cnt/cnt_index.php
@@ -14,7 +14,7 @@
   	<?php echo _("After one year of development, we are happy to announce the availability of pgAdmin3 beta, a complete rewrite of pgAdmin2 for Ms Windows, GNU/Linux and FreeBSD.");?>
     <?php echo _("We would like to thank our developer and translator community who contributed to the project.");?>
     <?php echo sprintf(_("pgAdmin3 is the result of a joint effort of a team of more than %d contributors from %d different countries."), 100, 26);?>
-    <?php echo sprintf(_("pgAdmin3 Beta-%d fixes several bugs as shown on the <a href='%s'>Change log</a>."), 2, "development.php#changelog");?>
+    <?php echo sprintf(_("pgAdmin3 Beta-%d fixes several bugs listed on the <a href='%s'>Change log</a> page."), 2, "development.php#changelog");?>
     <br><br>
   	<?php echo sprintf(_("pgAdmin3 is a free software project released under the <a href='%s'>Artistic licence</a>."), "cnt/licence.txt");?>
   	<?php echo sprintf(_("The software is available in source and binary format from the <a href='%s'>Download</a> page."), "download.php");?>

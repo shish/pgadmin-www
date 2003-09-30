28ea7f8ef08fd3af1269c1a5e9059a69e8713759
Jean-Michel Pouré <jm@poure.com>
2003-09-30 09:02:13 +0000

    Typo
    git-svn-id: svn://svn.pgadmin.org/trunk@2655 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_download.php
+++ pgadmin3/cnt/cnt_download.php
@@ -1,8 +1,8 @@
 <div class="sideBox LHS">
   <div><?php echo _("Download")?></div>
   <a href="#mirrors">&rsaquo; <?php echo _("List of mirrors")?></a>
-  <a href="#binaries">&rsaquo; <?php echo _("Binary packages")?></a>
-  <a href="#source">&rsaquo; <?php echo _("Source code")?></a>
+  <a href="#binaries">&rsaquo; <?php echo _("Installing binary packages")?></a>
+  <a href="#source">&rsaquo; <?php echo _("Installing from source")?></a>
 </div>
 
 <div id="bodyText">

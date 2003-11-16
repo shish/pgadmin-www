d6af351739c803303ce11c4d8b0708755b31371c
Andreas Pflug <pgadmin@pse-consulting.de>
2003-11-16 19:24:14 +0000

    make translation directly downloadable
    git-svn-id: svn://svn.pgadmin.org/trunk@2894 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/class/pgadmin_po.php
+++ pgadmin3/class/pgadmin_po.php
@@ -45,12 +45,12 @@ class po_status {
     }
 
     $_serialize = fread ($_handle, filesize ($_filename));
-		fclose ($_handle);
+    fclose ($_handle);
 
     $this->_content = array();
     $this->_content = unserialize($_serialize);
-   	reset ($this->_content);
-		$this->_size = count ($this->_content['locale']);
+    reset ($this->_content);
+    $this->_size = count ($this->_content['locale']);
   }
 
   function setWebRoot ($_webroot) {
@@ -58,11 +58,19 @@ class po_status {
   }
 
   function setCvsRoot ($_cvsroot) {
-  	if (file_exists($_cvsroot."/CVS/Repository")) {
-			$this->_cvsroot = $_cvsroot;
+    if (file_exists($_cvsroot."/CVS/Repository")) {
+	$this->_cvsroot = $_cvsroot;
     } else {
     	$this->_error = "Error: $_cvsroot is not a CVS repository.";
     }
+  }
+
+  function getLanguageTemplate() {
+    return "../" .str_replace ($this->_webroot ."/", "", $this->_cvsroot) . "/pgadmin3.pot";
+  }
+  
+  function getLanguageTotal() {
+    return $this->_content['po_total'][0];
   }
 
   function getNbLanguages () {
@@ -71,22 +79,24 @@ class po_status {
 
 
   function addLanguage ($_locale, $_language, $_translator_name, $_translator_email, $_po_name, $_po_path, $_pot_name, $_pot_path) {
-		$this->_content['locale'][$this->_size]    = $_locale;
-		$this->_content['language'][$this->_size]  = $_language;
+    $this->_content['locale'][$this->_size]    = $_locale;
+    $this->_content['language'][$this->_size]  = $_language;
     $this->_content['translator_name'][$this->_size]= $_translator_name;
     $this->_content['translator_email'][$this->_size]= $_translator_email;
 
-		$this->_content['pot_name'][$this->_size]   = $_pot_name;
-		$this->_content['pot_path'][$this->_size]  = $_pot_path;
+    $this->_content['pot_name'][$this->_size]   = "unused";     //$_pot_name;
+    $this->_content['pot_path'][$this->_size]  = "unused";      //$_pot_path;
+
     $_pot_stat = array();
-    $_pot_stat = $this->getStatistics ($this->_cvsroot . "/" . $_pot_path);
+    $_pot_stat = $this->getStatistics ($this->_cvsroot . "/pgadmin3.pot");
     $this->_content['pot_total'][$this->_size] = $_pot_stat['total'];
 
-    $this->_content['po_name'][$this->_size]   = $_po_name;
-    $this->_content['po_path'][$this->_size]   = $_po_path;
+    $this->_content['po_name'][$this->_size]   = "unused";      //$_po_name;
+    $this->_content['po_path'][$this->_size]   = "unused";      // $_po_path;
+
     $_po_stat = array();
-		$_po_stat = $this->getStatistics ($this->_cvsroot . "/" . $_po_path);
-		$this->_content['po_total'][$this->_size]        = $_po_stat['total'];
+    $_po_stat = $this->getStatistics ($this->_cvsroot . "/src/ui/" . $_locale . "/pgadmin3.po");
+    $this->_content['po_total'][$this->_size]        = $_po_stat['total'];
     $this->_content['po_translated'][$this->_size]   = $_po_stat['translated'];
     $this->_content['po_untranslated'][$this->_size] = $_po_stat['untranslated'] + $_po_stat['fuzzy'];
     $this->_content['po_status'][$this->_size]       = $_po_stat['status'];
@@ -125,122 +135,122 @@ class po_status {
   }
 
   function sortByLocale($_sort_order) {
-		array_multisort (
-    	$this->_content['locale'], $_sort_order, SORT_STRING,
-      $this->_content['language'],
-      $this->_content['translator_name'],
-      $this->_content['translator_email'],
-      $this->_content['pot_name'],
-      $this->_content['pot_path'],
-      $this->_content['pot_total'],
-      $this->_content['po_name'],
-      $this->_content['po_path'],
-      $this->_content['po_total'],
-      $this->_content['po_translated'],
-      $this->_content['po_untranslated'],
-      $this->_content['po_status']
+    array_multisort (
+        $this->_content['locale'], $_sort_order, SORT_STRING,
+        $this->_content['language'],
+        $this->_content['translator_name'],
+        $this->_content['translator_email'],
+        $this->_content['pot_name'],
+        $this->_content['pot_path'],
+        $this->_content['pot_total'],
+        $this->_content['po_name'],
+        $this->_content['po_path'],
+        $this->_content['po_total'],
+        $this->_content['po_translated'],
+        $this->_content['po_untranslated'],
+        $this->_content['po_status']
     );
   }
 
   function sortByLanguage($_sort_order) {
-		array_multisort (
-    	$this->_content['language'], $_sort_order, SORT_STRING,
-      $this->_content['locale'],
-      $this->_content['translator_name'],
-      $this->_content['translator_email'],
-      $this->_content['pot_name'],
-      $this->_content['pot_path'],
-      $this->_content['pot_total'],
-      $this->_content['po_name'],
-      $this->_content['po_path'],
-      $this->_content['po_total'],
-      $this->_content['po_translated'],
-      $this->_content['po_untranslated'],
-      $this->_content['po_status']
+    array_multisort (
+        $this->_content['language'], $_sort_order, SORT_STRING,
+        $this->_content['locale'],
+        $this->_content['translator_name'],
+        $this->_content['translator_email'],
+        $this->_content['pot_name'],
+        $this->_content['pot_path'],
+        $this->_content['pot_total'],
+        $this->_content['po_name'],
+        $this->_content['po_path'],
+        $this->_content['po_total'],
+        $this->_content['po_translated'],
+        $this->_content['po_untranslated'],
+        $this->_content['po_status']
     );
   }
 
   function sortByName($_sort_order) {
-		array_multisort (
-    	$this->_content['translator_name'], $_sort_order, SORT_STRING,
+    array_multisort (
+        $this->_content['translator_name'], $_sort_order, SORT_STRING,
     	$this->_content['locale'],
-      $this->_content['language'],
-      $this->_content['translator_email'],
-      $this->_content['pot_name'],
-      $this->_content['pot_path'],
-      $this->_content['pot_total'],
-      $this->_content['po_name'],
-      $this->_content['po_path'],
-      $this->_content['po_total'],
-      $this->_content['po_translated'],
-      $this->_content['po_untranslated'],
-			$this->_content['po_status']
+        $this->_content['language'],
+        $this->_content['translator_email'],
+        $this->_content['pot_name'],
+        $this->_content['pot_path'],
+        $this->_content['pot_total'],
+        $this->_content['po_name'],
+        $this->_content['po_path'],
+        $this->_content['po_total'],
+        $this->_content['po_translated'],
+        $this->_content['po_untranslated'],
+        $this->_content['po_status']
     );
   }
 
   function sortByTranslated($_sort_order) {
-		array_multisort (
-    	$this->_content['po_translated'], $_sort_order, SORT_NUMERIC,
-    	$this->_content['locale'],
-      $this->_content['language'],
-      $this->_content['translator_name'],
-      $this->_content['translator_email'],
-      $this->_content['pot_name'],
-      $this->_content['pot_path'],
-      $this->_content['pot_total'],
-      $this->_content['po_name'],
-      $this->_content['po_path'],
-      $this->_content['po_total'],
-      $this->_content['po_untranslated'],
-			$this->_content['po_status']
+    array_multisort (
+        $this->_content['po_translated'], $_sort_order, SORT_NUMERIC,
+        $this->_content['locale'],
+        $this->_content['language'],
+        $this->_content['translator_name'],
+        $this->_content['translator_email'],
+        $this->_content['pot_name'],
+        $this->_content['pot_path'],
+        $this->_content['pot_total'],
+        $this->_content['po_name'],
+        $this->_content['po_path'],
+        $this->_content['po_total'],
+        $this->_content['po_untranslated'],
+	$this->_content['po_status']
     );
   }
 
   function sortByUntranslated($_sort_order) {
-		array_multisort (
+    array_multisort (
     	$this->_content['po_untranslated'], $_sort_order, SORT_NUMERIC,
     	$this->_content['locale'],
-      $this->_content['language'],
-      $this->_content['translator_name'],
-      $this->_content['translator_email'],
-      $this->_content['pot_name'],
-      $this->_content['pot_path'],
-      $this->_content['pot_total'],
-      $this->_content['po_name'],
-      $this->_content['po_path'],
-      $this->_content['po_total'],
-      $this->_content['po_translated'],
-			$this->_content['po_status']
+        $this->_content['language'],
+        $this->_content['translator_name'],
+        $this->_content['translator_email'],
+        $this->_content['pot_name'],
+        $this->_content['pot_path'],
+        $this->_content['pot_total'],
+        $this->_content['po_name'],
+        $this->_content['po_path'],
+        $this->_content['po_total'],
+        $this->_content['po_translated'],
+        $this->_content['po_status']
     );
   }
 
   function sortByStatus($_sort_order) {
-		array_multisort (
-    	$this->_content['po_status'], $_sort_order, SORT_NUMERIC,
+    array_multisort (
+        $this->_content['po_status'], $_sort_order, SORT_NUMERIC,
     	$this->_content['locale'],
-      $this->_content['language'],
-      $this->_content['translator_name'],
-      $this->_content['translator_email'],
-      $this->_content['pot_name'],
-      $this->_content['pot_path'],
-      $this->_content['pot_total'],
-      $this->_content['po_name'],
-      $this->_content['po_path'],
-      $this->_content['po_total'],
-      $this->_content['po_translated'],
-      $this->_content['po_untranslated']
+        $this->_content['language'],
+        $this->_content['translator_name'],
+        $this->_content['translator_email'],
+        $this->_content['pot_name'],
+        $this->_content['pot_path'],
+        $this->_content['pot_total'],
+        $this->_content['po_name'],
+        $this->_content['po_path'],
+        $this->_content['po_total'],
+        $this->_content['po_translated'],
+        $this->_content['po_untranslated']
     );
   }
 
   function display ($_object) {
-  	reset ($this->_content);
-		$_result = "<table border=1 cellspacing=0 cellpadding=5>";
+    reset ($this->_content);
+    $_result = "<table border=1 cellspacing=0 cellpadding=5>";
     $_result = $_result . "<tr bgcolor=lightgray >";
     $_result = $_result . "<th><font color=black> <a href='?".$_object."_poSortBy=locale'>". _("Locale") ."</a> </font></th>";
     $_result = $_result . "<th><font color=black> <a href='?".$_object."_poSortBy=language'>". _("Language") ."</a> </font></th>";
     $_result = $_result . "<th><font color=black> <a href='?".$_object."_poSortBy=name'>". _("Author") ."<br>". _("Maintainer")."</a></td> </font></th>";
-    $_result = $_result . "<th><font color=black> ". _("Download Template") ."</font></th>";
-    $_result = $_result . "<th><font color=black> ". _("Download Language") ."</font></th>";
+    $_result = $_result . "<th><font color=black> ". _("Download Translation") ."</font></th>";
+    $_result = $_result . "<th><font color=black> ". _("Download Source") ."</font></th>";
     $_result = $_result . "<th><font color=black> ". _("Total") ."</font></th>";
     $_result = $_result . "<th><font color=black> <a href='?".$_object."_poSortBy=translated'>". _("Translated")."</a> </font></th>";
     $_result = $_result . "<th><font color=black> <a href='?".$_object."_poSortBy=untranslated'>". _("Untranslated")."<br>". _("Fuzzy")."</a> </font></th>";
@@ -257,27 +267,28 @@ class po_status {
 				$_colour = "white";
       }
 
-      $_pot_name  = $this->_content['pot_name'][$_count];
       $_pot_total = $this->_content['pot_total'][$_count];
-      $_pot_path  =  "../" .str_replace ($this->_webroot ."/" , "", $this->_cvsroot . "/". $this->_content['pot_path'][$_count]);
 
-      $_po_name       = $this->_content['po_name'][$_count];
       $_po_total      = $this->_content['po_total'][$_count];
       $_po_untranslated = $this->_content['po_untranslated'][$_count];
       $_po_translated = $this->_content['po_translated'][$_count];
       $_po_status     = $this->_content['po_status'][$_count]."%";
-			$_po_path       =  "../" .str_replace ($this->_webroot ."/", "", $this->_cvsroot . "/" . $this->_content['po_path'][$_count]);
 
       $_locale           = $this->_content['locale'][$_count];
       $_language         = $this->_content['language'][$_count];
-			$_translator_email = $this->_content['translator_email'][$_count];
+      $_translator_email = $this->_content['translator_email'][$_count];
       $_translator_name  = $this->_content['translator_name'][$_count];
+
+      $_mo_name  = $_locale . "/pgadmin3.mo";
+      $_po_name  = $_locale . "/pgadmin3.po";
+      $_mo_path = "../" .str_replace ($this->_webroot ."/", "", $this->_cvsroot) . "/src/ui/" . $_locale . "/pgadmin3.mo";
+      $_po_path = "../" .str_replace ($this->_webroot ."/", "", $this->_cvsroot) . "/src/ui/" . $_locale . "/pgadmin3.po";
 
       $_result = $_result . "<tr bgcolor=$_colour>" .
       "<td>$_locale</td>" .
       "<td>$_language</td>" .
       "<td><a href='mailto:$_translator_email'>$_translator_name</a></td>" .
-      "<td><a href='$_pot_path'>$_pot_name</a></td>" .
+      "<td><a href='$_mo_path'>$_mo_name</a></td>" .
       "<td><a href='$_po_path'>$_po_name</a></td>" .
       "<td>" .$_po_total ."</td>" .
       "<td>". $_po_translated ."</td>" .

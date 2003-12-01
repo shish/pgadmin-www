a1e4428cec958c7d593d73b093dfb29c4de055f8
Jean-Michel Pouré <jm@poure.com>
2003-12-01 15:05:45 +0000

    Fixing XML
    git-svn-id: svn://svn.pgadmin.org/trunk@2970 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/tpl/menu.php
+++ pgadmin3/tpl/menu.php
@@ -8,16 +8,16 @@
 
       <td align="<?php echo $_SESSION['LANG_RIGHT']?>" valign="bottom">
       	Languages:
-        <a href="?locale=de_DE"><img src="img/flags/Germany.gif" alt="German">
-        <a href="?locale=en_US"><img src="img/flags/UK.gif" alt="English">
-        <a href="?locale=fa_IR"><img src="img/flags/Persian.gif" alt="Persian">
-        <a href="?locale=fr_FR"><img src="img/flags/France.gif" alt="France">
-        <a href="?locale=id_ID"><img src="img/flags/Indonesia.gif" alt="Indonesian">
-        <a href="?locale=ja_JP"><img src="img/flags/Japan.gif" alt="Japanese">
-        <a href="?locale=ru_RU"><img src="img/flags/Russia.gif" alt="Russian">
-        <a href="?locale=tr_TR"><img src="img/flags/Turkey.gif" alt="Turkish">
-        <a href="?locale=zh_CN"><img src="img/flags/China.gif" alt="Simplified Chinese">
-        <a href="?locale=zh_TW"><img src="img/flags/Taiwan.gif" alt="Traditional Chinese">
+        <a href="?locale=de_DE"><img src="img/flags/Germany.gif" alt="German"></a>
+        <a href="?locale=en_US"><img src="img/flags/UK.gif" alt="English"></a>
+        <a href="?locale=fa_IR"><img src="img/flags/Persian.gif" alt="Persian"></a>
+        <a href="?locale=fr_FR"><img src="img/flags/France.gif" alt="France"></a>
+        <a href="?locale=id_ID"><img src="img/flags/Indonesia.gif" alt="Indonesian"></a>
+        <a href="?locale=ja_JP"><img src="img/flags/Japan.gif" alt="Japanese"></a>
+        <a href="?locale=ru_RU"><img src="img/flags/Russia.gif" alt="Russian"></a>
+        <a href="?locale=tr_TR"><img src="img/flags/Turkey.gif" alt="Turkish"></a>
+        <a href="?locale=zh_CN"><img src="img/flags/China.gif" alt="Simplified Chinese"></a>
+        <a href="?locale=zh_TW"><img src="img/flags/Taiwan.gif" alt="Traditional Chinese"></a>
       </td>
 	</tr>
 

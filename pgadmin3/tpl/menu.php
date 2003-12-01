59df6e7787f2cb12f95ddc67d1c60d8301b792ed
Jean-Michel Pouré <jm@poure.com>
2003-12-01 15:22:17 +0000

    Fixing IMG tags
    git-svn-id: svn://svn.pgadmin.org/trunk@2973 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/tpl/menu.php
+++ pgadmin3/tpl/menu.php
@@ -8,16 +8,16 @@
 
       <td align="<?php echo $_SESSION['pgadmin']['locale_right']?>" valign="bottom">
       	Languages:
-        <a href="?locale=de_DE"><img src="img/flags/Germany.gif" alt="German"></a>
-        <a href="?locale=en_US"><img src="img/flags/UK.gif" alt="English"></a>
-        <a href="?locale=fa_IR"><img src="img/flags/Persian.gif" alt="Persian"></a>
-        <a href="?locale=fr_FR"><img src="img/flags/France.gif" alt="France"></a>
-        <a href="?locale=id_ID"><img src="img/flags/Indonesia.gif" alt="Indonesian"></a>
-        <a href="?locale=ja_JP"><img src="img/flags/Japan.gif" alt="Japanese"></a>
-        <a href="?locale=ru_RU"><img src="img/flags/Russia.gif" alt="Russian"></a>
-        <a href="?locale=tr_TR"><img src="img/flags/Turkey.gif" alt="Turkish"></a>
-        <a href="?locale=zh_CN"><img src="img/flags/China.gif" alt="Simplified Chinese"></a>
-        <a href="?locale=zh_TW"><img src="img/flags/Taiwan.gif" alt="Traditional Chinese"></a>
+        <a href="?locale=de_DE"><img src="img/flags/Germany.gif" alt="German"></img></a>
+        <a href="?locale=en_US"><img src="img/flags/UK.gif" alt="English"></img></a>
+        <a href="?locale=fa_IR"><img src="img/flags/Persian.gif" alt="Persian"></img></a>
+        <a href="?locale=fr_FR"><img src="img/flags/France.gif" alt="France"></img></a>
+        <a href="?locale=id_ID"><img src="img/flags/Indonesia.gif" alt="Indonesian"></img></a>
+        <a href="?locale=ja_JP"><img src="img/flags/Japan.gif" alt="Japanese"></img></a>
+        <a href="?locale=ru_RU"><img src="img/flags/Russia.gif" alt="Russian"></img></a>
+        <a href="?locale=tr_TR"><img src="img/flags/Turkey.gif" alt="Turkish"></img></a>
+        <a href="?locale=zh_CN"><img src="img/flags/China.gif" alt="Simplified Chinese"></img></a>
+        <a href="?locale=zh_TW"><img src="img/flags/Taiwan.gif" alt="Traditional Chinese"></img></a>
       </td>
 	</tr>
 

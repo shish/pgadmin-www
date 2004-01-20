bff8ed18b97bdb3e2bca887e3b2a2289aecda965
Jean-Michel Pouré <jm@poure.com>
2004-01-20 14:11:19 +0000

    Adding advocacy page.
    git-svn-id: svn://svn.pgadmin.org/trunk@3037 a7884b65-44f6-0310-8a51-81a127f17b15

---
+++ pgadmin3/cnt/cnt_advocacy.php
@@ -0,0 +1,50 @@
+<div class="sideBox LHS">
+<div><?php echo _("Advocacy");?></div>
+<a href="#introduction">&rsaquo; <?php echo _("Introduction");?></a>
+<a href="#list">&rsaquo; <?php echo _("Registration list");?></a>
+<a href="#press">&rsaquo; <?php echo _("Press articles");?></a>
+</div>
+
+<div id="bodyText">
+  <dd>
+    <?php echo _("The advocacy page is intended for the promotion of pgAdmin.");?>
+    <?php echo _("This page is only available in English.");?>
+  </dd>
+
+<h1 id="introduction">Introduction</h1>
+<dl>
+  <dt>Why promote pgAdmin?</dt>
+  <dd>Promoting free software is essential for economical development and civil liberties.
+  PostgreSQL and pgAdmin free software are used worldwide by a large number of end-users, companies and institutions. If you can spend an hour or more promoting us, it is certainly not a loss of time.
+  </dd>
+ <dd>
+  On the long run, reaching new users can help pgAdmin project get in touch with talented developers, who will in turn bring exciting new features to pgAdmin and possibly to PostgreSQL too.
+  </dd>
+
+    <dt>Promotion strategies</dt>
+  <dd>
+        The proposed strategy is quite simple :
+        <ul>
+          <li>Enhance the number of web sites pointing at pgAdmin web site. We are highly interested in registrating pgAdmin on free sofware websites. Every country has one or two major websites designed by free software 'opinion leaders'. To give an example, just visit <a href='http://freshmeat.net/projects/pgadmin3/?topic_id=68'>Freshmeat entry</a> for pgAdmin.
+          <li>Contact the press and ask journalists to write short articles about pgAdmin.
+          <li>Help us a few hours every month, but constantly during years.
+          <li>Analyse the <a href='http://www.pgadmin.org/stats/webalizer/'>statistics</a> of pgAdmin3 website in your country, which reflect the overall interest for pgAdmin.
+        </ul>
+  </dd>
+</dl>
+
+<a class="topOfPage" href="#top" title="Top Of Page">top</a>
+
+<h1 id="list">Registration list</h1>
+<dl>
+</dl>
+
+<a class="topOfPage" href="#top" title="Top Of Page">top</a>
+
+<h1 id="press">Press articles</h1>
+<dl>
+</dl>
+
+<a class="topOfPage" href="#top" title="Top Of Page">top</a>
+
+</div>

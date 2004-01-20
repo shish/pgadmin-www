d142aa521bc3686720d0f7c4c04369d770b8a931
Jean-Michel Pouré <jm@poure.com>
2004-01-20 15:19:25 +0000

    Minor changes.
    git-svn-id: svn://svn.pgadmin.org/trunk@3050 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_advocacy.php
+++ pgadmin3/cnt/cnt_advocacy.php
@@ -15,7 +15,7 @@
 <dl>
   <dt>Why promote pgAdmin?</dt>
   <dd>
-  Reaching new users can help pgAdmin project get in touch with talented developers, who will in turn bring exciting new features to pgAdmin and possibly to PostgreSQL too.
+  Promotion is essential in the life and development of the pgAdmin project. Reaching new users can help pgAdmin project get in touch with talented developers, who may bring exciting new features to pgAdmin and possibly to PostgreSQL too.
   </dd>
   <dd>On the long run, promoting free software is essential for economical development and civil liberties.
   PostgreSQL and pgAdmin free software are used worldwide by a large number of end-users, companies, governments and institutions. Therefore, helping us promote pgAdmin is certainly not a loss of time.

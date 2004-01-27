13abd883502a062ad23bdd11e857b8da2c2a12dd
Jean-Michel Pouré <jm@poure.com>
2004-01-27 13:49:15 +0000

    Adding freewarefiles
    git-svn-id: svn://svn.pgadmin.org/trunk@3071 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_advocacy.php
+++ pgadmin3/cnt/cnt_advocacy.php
@@ -55,6 +55,7 @@ en (English)
 <dd>
   <ul>
     <li><a href='http://dmoz.org/Computers/Software/Databases/PostgreSQL/'>dmoz</a> (updating pending)
+    <li><a href='http://www.freewarefiles.com'>Freewarefiles</a> (pending)
     <li><a href='http://freshmeat.net/projects/pgadmin3/?topic_id=68'>Freshmeat</a>
     <li><a href='http://gborg.postgresql.org/project/pgadmin3/projdisplay.php'>Gborg</a>
     <li><a href='http://www.hotscripts.com/Tools_and_Utilities/Database_Tools'>Hotscripts</a> (pending)

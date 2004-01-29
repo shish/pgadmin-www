705e15813269a009cb249a4b0d3c755e9040cc06
Jean-Michel Pouré <jm@poure.com>
2004-01-29 09:59:44 +0000

    Removing Lune Rouge
    git-svn-id: svn://svn.pgadmin.org/trunk@3115 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_advocacy.php
+++ pgadmin3/cnt/cnt_advocacy.php
@@ -124,7 +124,6 @@ fr (French)
     <li><a href='http://www.jesuislibre.org/applications/Bases_de_Donnees/Gestionnaire/pgAdmin_III.php3'>Je suis libre</a></li>
     <li><a href='http://www.framasoft.net/rubrique2.html'>Framasoft</a> (pending)</li>
     <li><a href='http://gnuwin.epfl.ch/fr/index.html'>GNU Win</a> (pending)</li>
-    <li><a href='http://www.lunerouge.org/gnu/applis_f.ht'>Lune Rouge</a> (pending)</li>
   </ul>
 </dd>
 

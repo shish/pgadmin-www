5d854d4ebd7131f3defbafcb89c76d5bdce0c0a2
Jean-Michel Pouré <jm@poure.com>
2004-02-03 23:12:31 +0000

    Adding various German suggestions
    git-svn-id: svn://svn.pgadmin.org/trunk@3176 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_advocacy.php
+++ pgadmin3/cnt/cnt_advocacy.php
@@ -131,10 +131,10 @@ cs (Czech)
 <dd>
 de (German)
   <ul>
-    <li><a href='http://www.apps4linux.org'></a>Apps4Linux (reminder to register)</li>
+    <li><a href='http://www.apps4linux.org'></a>Apps4Linux</a> (reminder to register)</li>
     <li><a href='http://www.dmoz.org/World/Deutsch/Computer/Software/Datenbanken/PostgreSQL'>DMOZ</a></li>
-    <li><a href='http://www.links2linux.de'></a>links2linux.de (reminder to register)</li>
-    <li><a href='http://www.linuxsoftboard.de/viewforum.php?f=29'></a>LinuxSoftBoard (reminder to register)</li>
+    <li><a href='http://www.links2linux.de'></a>links2linux.de</a> (reminder to register)</li>
+    <li><a href='http://www.linuxsoftboard.de/viewforum.php?f=29'></a>LinuxSoftBoard</a> (reminder to register)</li>
   </ul>
 </dd>
 

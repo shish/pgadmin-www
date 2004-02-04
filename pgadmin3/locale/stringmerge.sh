465c07e84d4951840720be06bad37d9c547a7ef9
Jean-Michel Pouré <jm@poure.com>
2004-02-04 09:20:31 +0000

    Update
    git-svn-id: svn://svn.pgadmin.org/trunk@3188 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/locale/stringmerge.sh
+++ pgadmin3/locale/stringmerge.sh
@@ -17,14 +17,13 @@ if test -f pgadmin3_website.pot; then
   for GETTEXTDIR in *_*/LC_MESSAGES/ ; do
     echo "Entering $GETTEXTDIR."
     cd $GETTEXTDIR
-    echo "Updating from CVS."
     echo "Merging pgadmin3_website.pot into pgadmin3_website.po"
-    msgmerge --update pgadmin3_website.po ../../../pgadmin3_website.pot
+    msgmerge --update pgadmin3_website.po ../../pgadmin3_website.pot
     echo "Compiling pgadmin3.mo"
     msgfmt -o pgadmin3_website.mo pgadmin3_website.po
     echo "Committing changes to CVS."
     #cvs commit -m "Automatic merge using stringmerge script." pgadmin3_website.po
     #cvs commit -m "Automatic merge using stringmerge script." pgadmin3_website.mo
-    cd ../../..
+    cd ../..
   done
 fi

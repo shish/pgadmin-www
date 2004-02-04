0421fa71278313e823c6162dc2905e4dcc95f784
Jean-Michel Pouré <jm@poure.com>
2004-02-04 09:15:02 +0000

    New merge script
    git-svn-id: svn://svn.pgadmin.org/trunk@3185 a7884b65-44f6-0310-8a51-81a127f17b15

---
+++ pgadmin3/locale/stringmerge.sh
@@ -0,0 +1,30 @@
+#######################################################################
+#
+# pgAdmin III - PostgreSQL Tools
+# Copyright (C) 2002 - 2003, The pgAdmin Development Team
+# This software is released under the Artistic Licence
+#
+# stringmerge - merge pgadmin3.pot template file into existing pgadmin3.po
+# translated files, compile and publish them on CVS.
+#
+# Same as 'Update from .pot' feature of poEdit.
+#
+#######################################################################
+
+if test -f pgadmin3_website.pot; then
+
+  for GETTEXTDIR in *_*/LC_MESSAGES/* ; do
+  	echo "Entering $GETTEXTDIR."
+    cd $GETTEXTDIR
+    echo "Updating from CVS."
+    cvs update -C -P -d
+    echo "Merging pgadmin3_website.pot into pgadmin3_website.po"
+    msgmerge --update pgadmin3_website.po ../../../pgadmin3_website.pot
+    echo "Compiling pgadmin3.mo"
+    msgfmt -o pgadmin3_website.mo pgadmin3_website.po
+    echo "Committing changes to CVS."
+    cvs commit -m "Automatic merge using stringmerge script." pgadmin3_website.po
+    cvs commit -m "Automatic merge using stringmerge script." pgadmin3_website.mo
+    cd ../../..
+  done
+fi

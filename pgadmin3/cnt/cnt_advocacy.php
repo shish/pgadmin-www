bde43090218dd66f472b0de3825642b1f5e15868
Jean-Michel Pouré <jm@poure.com>
2004-02-11 14:23:01 +0000

    Confirming Framasoft
    git-svn-id: svn://svn.pgadmin.org/trunk@3304 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_advocacy.php
+++ pgadmin3/cnt/cnt_advocacy.php
@@ -174,7 +174,7 @@ fr (French)
   <ul>
     <li><a href='http://www.anshare.com/fiche.asp?I=14713'>Anshare</a></li>
     <li><a href='http://www.jesuislibre.org/applications/Bases_de_Donnees/Gestionnaire/pgAdmin_III.php3'>Je suis libre</a></li>
-    <li><a href='http://www.framasoft.net/rubrique2.html'>Framasoft</a> (pending)</li>
+    <li><a href='http://www.framasoft.net/article2239.html'>Framasoft</a></li>
     <li><a href='http://gnuwin.epfl.ch/fr/index.html'>GNU Win</a> (pending)</li>
     <li><a href='http://telecharger.01net.com/windows/Programmation/base_de_donne'>Telecharger.com</a> (pending)</li>
   </ul>

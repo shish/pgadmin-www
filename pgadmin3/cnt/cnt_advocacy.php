18eb5acdb3bcc88af1b3e855d16da07154452cbc
Jean-Michel Pouré <jm@poure.com>
2004-02-18 10:11:51 +0000

    Confirm TopDownload
    git-svn-id: svn://svn.pgadmin.org/trunk@3326 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_advocacy.php
+++ pgadmin3/cnt/cnt_advocacy.php
@@ -155,7 +155,7 @@ en (English)
     <li><a href='http://www.scriptz.com/details/12264.html'>Scriptz</a></li>
     <li><a href='http://www.sofotex.com/pgAdmin-III-for-PostgreSQL-download_L17399.html'>Sofotex</a></li>
     <li><a href='http://www.theopencd.org'>The Open CD</a> (reminder to register)</li>
-    <li><a href='http://www.topdownloads.net/software/browse.php?category=112'>Top Downloads Net</a> (pending)</li>
+    <li><a href='http://www.topdownloads.net/software/view.php?id=14174'>Top Downloads Net</a></li>
     <li><a href='http://www.tucows.com'>Tucows</a> (pending)</li>
     <li><a href='http://www.winpicks.com'>WinPick</a> (pending)</li>
     <li><a href='http://www.zend.com/apps.php?CID=56'>Zend</a></li>

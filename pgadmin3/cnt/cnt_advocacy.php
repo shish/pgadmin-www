d81f479f7769d25451dcd0029e9c858d0db02fe1
Jean-Michel Pouré <jm@poure.com>
2004-02-18 13:56:13 +0000

    Adding new entries
    git-svn-id: svn://svn.pgadmin.org/trunk@3330 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_advocacy.php
+++ pgadmin3/cnt/cnt_advocacy.php
@@ -155,12 +155,14 @@ en (English)
     <li><a href='http://www.programfiles.com/Default.asp?CatId=962'>ProgramFiles.com</a> (pending)</li>
     <li><a href='http://www.programmersheaven.com/search/download.asp?FileID=32608'>Programmers Heaven</a></li>
     <li><a href='http://www.scriptz.com/details/12264.html'>Scriptz</a></li>
+    <li><a href='http://www.snapfiles.com/freeware/freeware.html'> Snapfiles</a> (Reminder to register)</li>
     <li><a href='http://www.sofotex.com/pgAdmin-III-for-PostgreSQL-download_L17399.html'>Sofotex</a></li>
     <li><a href='http://www.theopencd.org'>The Open CD</a> (reminder to register)</li>
     <li><a href='http://www.topdownloads.net/software/view.php?id=14174'>Top Downloads Net</a></li>
     <li><a href='http://www.tucows.com'>Tucows</a> (pending)</li>
     <li><a href='http://www.winpicks.com/ShowDetailsRed.asp?title=pgAdmin+III'>WinPick</a></li>
     <li><a href='http://www.zend.com/apps.php?CID=56'>Zend</a></li>
+    <li><a href='http://zeoslib.sourceforge.net/modules.php?name=Web_Links&l_op=search&query=pgadmin'>Zeos Library</a></li>
   </ul>
 </dd>
 

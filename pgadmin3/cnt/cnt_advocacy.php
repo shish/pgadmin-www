4591bb52fcf294c08d5e0c4005f11dd5768467a1
Jean-Michel Pouré <jm@poure.com>
2004-02-26 14:14:42 +0000

    Confirmation of WorldSSP
    git-svn-id: svn://svn.pgadmin.org/trunk@3363 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_advocacy.php
+++ pgadmin3/cnt/cnt_advocacy.php
@@ -171,7 +171,7 @@ en (English)
     <li><a href='http://www.tucows.com'>Tucows</a> (pending)</li>
     <li><a href='http://www.webmasterfree.com'>Webmaster Free</a> (pending)</li>
     <li><a href='http://www.winpicks.com/ShowDetailsRed.asp?title=pgAdmin+III'>WinPick</a></li>
-    <li><a href='http://www.worldssp.net'>WolrdSSP</a> (pending)</li>
+    <li><a href='http://www.worldssp.net/detailview.asp?proid=12563'>WolrdSSP</a></li>
     <li><a href='http://www.zend.com/apps.php?CID=56'>Zend</a></li>
     <li><a href='http://zeoslib.sourceforge.net/modules.php?name=Web_Links&l_op=search&query=pgadmin'>Zeos Library</a></li>
   </ul>

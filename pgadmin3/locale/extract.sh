eb5e31633a15bd0b7b1995416efcebfd0a83b9ce
Jean-Michel Pouré <jm@poure.com>
2004-03-28 12:39:44 +0000

    Updated language script in order to extract text only from specific pages.
    git-svn-id: svn://svn.pgadmin.org/trunk@3426 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/locale/extract.sh
+++ pgadmin3/locale/extract.sh
@@ -1,7 +1,14 @@
-xgettext    -s --keyword=_ --language=C --output=pgadmin3_website.pot ../cnt/cnt_*.php
+xgettext    -s --keyword=_ --language=C --output=pgadmin3_website.pot ../cnt/cnt_advocacy.php
+xgettext -j -s --keyword=_ --language=C --output=pgadmin3_website.pot ../cnt/cnt_development.php
+xgettext -j -s --keyword=_ --language=C --output=pgadmin3_website.pot ../cnt/cnt_download.php
+xgettext -j -s --keyword=_ --language=C --output=pgadmin3_website.pot ../cnt/cnt_index.php
+xgettext -j -s --keyword=_ --language=C --output=pgadmin3_website.pot ../cnt/cnt_mirrors.php
+xgettext -j -s --keyword=_ --language=C --output=pgadmin3_website.pot ../cnt/cnt_screenshots.php
+xgettext -j -s --keyword=_ --language=C --output=pgadmin3_website.pot ../cnt/cnt_support.php
+xgettext -j -s --keyword=_ --language=C --output=pgadmin3_website.pot ../cnt/cnt_translation.php
+
 xgettext -j -s --keyword=_ --language=C --output=pgadmin3_website.pot ../tpl/menu.php
 xgettext -j -s --keyword=_ --language=C --output=pgadmin3_website.pot ../class/pgadmin_po.php
 recode latin1..u8 pgadmin3_website.pot
 sed -e "s/=CHARSET/=UTF-8/g" pgadmin3_website.pot > temp.pot
 mv -f temp.pot pgadmin3_website.pot
-

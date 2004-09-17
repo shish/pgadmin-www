9086ac5dddedb76bea7b61b95632764b29620ee8
Andreas Pflug <pgadmin@pse-consulting.de>
2004-09-17 09:43:00 +0000

    packager mail
    git-svn-id: svn://svn.pgadmin.org/trunk@3691 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_download.php
+++ pgadmin3/cnt/cnt_download.php
@@ -20,7 +20,7 @@
   <h1 id="debian"><?php echo _("Debian packages")?></h1>
   <dl>
     <dt>
-      <?php echo  sprintf(_("Packager: %s"), "Raphaël Enrici");?>
+      <?php echo  sprintf(_("Packager: %s"), "<a href='mailto:blacknoz@freesurf.fr'>Raphaël Enrici</A>");?>
     </dt>
 
     <dd>

96ef9a4280c5a4c36c389b497aa804ae7bdc57a9
Dave Page <dpage@pgadmin.org>
2004-11-12 13:38:25 +0000

    A-ha. Someone else's broken HTML this time!!
    git-svn-id: svn://svn.pgadmin.org/trunk@3853 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_translation.php
+++ pgadmin3/cnt/cnt_translation.php
@@ -73,8 +73,8 @@
   <a class="topOfPage" href="#top" title="Top Of Page">top</a>
   <br>
   <h1 id="vacant"><?php echo sprintf(_("Out of date Translations (%d languages)"), $outofdate->getNbLanguages());?></h1>
-	<?php echo sprintf(_("The translations below are not considered complete enough for inclusion in current releases."));?></h1>
-	<?php echo sprintf(_("If you like to continue the work, please don't hesitate to contact the current maintainer (please CC pgadmin-hackers@pgadmin.org)!"));?></h1>
+	<?php echo sprintf(_("The translations below are not considered complete enough for inclusion in current releases."));?>
+	<?php echo sprintf(_("If you like to continue the work, please don't hesitate to contact the current maintainer (please C pgadmin-hackers@pgadmin.org)!"));?>
   <br>
     <?php
     	$_sortBy = $_SESSION['pgadmin']['d3456_poSortBy'];

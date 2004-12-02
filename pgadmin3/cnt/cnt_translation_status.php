a7ac0b1938cfdf54c386de1544de0716b88b0ef2
Dave Page <dpage@pgadmin.org>
2004-12-02 08:57:48 +0000

    Arabic translation is being maintained again :-)
    git-svn-id: svn://svn.pgadmin.org/trunk@3895 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_translation_status.php
+++ pgadmin3/cnt/cnt_translation_status.php
@@ -14,7 +14,7 @@
 //
 
   $translated->addLanguage ("af_ZA", "Afrikaans", "Petri Jooste", "rkwjpj@puknet.puk.ac.za", "pgadmin3.po","src/ui/af_ZA/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
-  $outofdate->addLanguage ("ar_SA", "Arabic", "Unmaintained", "pgadmin-hackers@postgresql.org", "pgadmin3.po","src/ui/ar_SA/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
+  $translated->addLanguage ("ar_SA", "Arabic", "Ossama Khayat", "okhayat@yahoo.com", "pgadmin3.po","src/ui/ar_SA/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $outofdate->addLanguage ("bg_BG", "Bulgarian", "Latchezar Kostov", "lkostov_hobby@yahoo.co.uk", "pgadmin3.po","src/ui/bg_BG/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $translated->addLanguage ("ca_ES", "Catalan", "Carme Cerdà Torres", "carmect@tv3mail.com", "pgadmin3.po","src/ui/ca_ES/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $outofdate->addLanguage ("cs_CZ", "Czech", "Daniel Horák", "dan.horak@email.cz", "pgadmin3.po","src/ui/cs_CZ/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");

465617c9810c9475efb318c47a1e9d08bc203259
Dave Page <dpage@pgadmin.org>
2005-06-16 08:01:46 +0000

    Update download page for current PostgreSQL mirror organisation.
    git-svn-id: svn://svn.pgadmin.org/trunk@4303 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_download.php
+++ pgadmin3/cnt/cnt_download.php
@@ -1,14 +1,14 @@
 <div class="sideBox LHS">
-  <div><?php echo _("Download")?></div>
-  <a href="#debian">&rsaquo; <?php echo _("Debian")?></a>
-  <a href="#rpm">&rsaquo; <?php echo _("RPM")?></a>
-  <a href="#slackware">&rsaquo; <?php echo _("Slackware")?></a>
-  <a href="#freebsd">&rsaquo; <?php echo _("FreeBSD")?></a>
-  <a href="#win32">&rsaquo; <?php echo _("Ms Windows")?>
-  <a href="#source">&rsaquo; <?php echo _("Installing pgAdmin III from source code")?></a>
-  <a href="#snapshots">&rsaquo; <?php echo _("Installing pgAdmin III from binary snapshots");?></a>
-  <a href="#mirrors">&rsaquo; <?php echo _("Download mirrors")?></a>
-  <a href="#languages">&rsaquo;<?php echo _("Additional languages")?></a>
+    <div><?php echo _("Download")?></div>
+    <a href="#debian">&rsaquo; <?php echo _("Debian")?></a>
+    <a href="#rpm">&rsaquo; <?php echo _("RPM")?></a>
+    <a href="#slackware">&rsaquo; <?php echo _("Slackware")?></a>
+    <a href="#freebsd">&rsaquo; <?php echo _("FreeBSD")?></a>
+    <a href="#win32">&rsaquo; <?php echo _("Ms Windows")?>
+    <a href="#osx">&rsaquo; <?php echo _("Mac OSX")?>
+    <a href="#source">&rsaquo; <?php echo _("Installing pgAdmin III from source code")?></a>
+    <a href="#snapshots">&rsaquo; <?php echo _("Installing pgAdmin III from binary snapshots");?></a>
+    <a href="#languages">&rsaquo;<?php echo _("Additional languages")?></a>
 </div>
 
 <div id="bodyText">
@@ -40,9 +40,9 @@
 		<?php echo _("For Debian Sarge (stable):");?>
 		<br/>
 		<?php echo ("An APT repository is provided to ease installation.");?>
-		<?php echo sprintf(_("Before you start installation, please choose a mirror in the list of our <a href='%s'>mirrors</a>."), "#mirrors");?>
-		<?php echo "You only need to right-click on a mirror and copy its URL.";?>
-			<?php echo "On each mirror, the packages are located in:";?>
+		<?php echo sprintf(_("Before you start installation, please choose a mirror in the list of our <a href=\"%s\" target=\"_new\">mirrors</a>."), "http://wwwmaster.postgresql.org/download/mirrors-ftp");?>
+		<?php echo "Click one of the flags, and copy the URL you are redirected to.";?>
+        <?php echo "On each mirror, the packages are located in:";?>
 		
 			<table width= '100%' cellspacing=0 cellpadding=10 border=0>
 		<tr bgcolor=beige>
@@ -77,9 +77,9 @@
             <?php echo _("For Debian Etch (testing):");?>
 	    <br/>
 	    <?php echo ("An APT repository is provided to ease installation.");?>
-      <?php echo sprintf(_("Before you start installation, please choose a mirror in the list of our <a href='%s'>mirrors</a>."), "#mirrors");?>
-      <?php echo "You only need to right-click on a mirror and copy its URL.";?>
-	  <?php echo "On each mirror, the packages are located in:";?>
+		<?php echo sprintf(_("Before you start installation, please choose a mirror in the list of our <a href=\"%s\" target=\"_new\">mirrors</a>."), "http://wwwmaster.postgresql.org/download/mirrors-ftp");?>
+        <?php echo "Click one of the flags, and copy the URL you are redirected to.";?>
+	    <?php echo "On each mirror, the packages are located in:";?>
 
 		<table width= '100%' cellspacing=0 cellpadding=10 border=0>
 		<tr bgcolor=beige>
@@ -132,219 +132,122 @@
 
    </dd>
 </dl>
+<a class="topOfPage" href="#top" title="Top Of Page">top</a>
 
-  <a class="topOfPage" href="#top" title="Top Of Page">top</a>
+    <h1 id="rpm"><?php echo "RPM packages";?></h1>
+    <dl>
+        <dt><?php echo  sprintf(_("Packager: %s"), "<a href='mailto:jm@poure.com'>Jean-Michel Pouré</a> / <a href='mailto:devrim@gunduz.org'>Devrim Gündüz</a> ");?></dt>
 
-  <h1 id="rpm"><?php echo "RPM packages";?></h1>
-  <dl>
-    <dt><?php echo  sprintf(_("Packager: %s"), "<a href='mailto:jm@poure.com'>Jean-Michel Pouré</a> / <a href='mailto:devrim@gunduz.org'>Devrim Gündüz</a> ");?></dt>
+        <dd>
+            <?php echo _("pgAdmin III RPM packages are available for several GNU/Linux distributions from the <a href=\"%s\">PostgreSQL mirror network</a>."), "http://www.postgresql.org/ftp/pgadmin3/release/");?>
+        </dd>
 
-    <dd>
-    <?php echo sprintf(_("pgAdmin III RPM packages are available for several GNU/Linux distributions, including Fedora %s, Mandrake %s, RedHat %s and SuSE %s."), "FC1 and FC2", "9.1", "9", "8.2");?>
- </dd>
+        <dd>
 
-  <dd>
-        <?php echo sprintf(_("Download pgAdmin package from our <a href='%s'>mirrors</a>."), "#mirrors");?>
-	<?php echo "On each mirror, the packages are located in:";?>
-
-
-	   <table width= '100%' cellspacing=0 cellpadding=10 border=0>
-      <tr bgcolor=beige>
-        <td><?php echo _("Location")?></td>
-        <td></td>
-        <td><?php echo _("Description")?></td>
-      </tr>
-
-      <tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/[VERSION]/FC3</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "GNU/Linux Fedora FC3")?></td>
-      </tr>
-      
-      <tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/[VERSION]/FC2</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "GNU/Linux Fedora FC2")?></td>
-      </tr>
-
-      <tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/[VERSION]/FC1</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "GNU/Linux Fedora FC1")?></td>
-      </tr>
-
-      <tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/[VERSION]/mandrake91</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "GNU/Linux Mandrake 9.1")?></td>
-      </tr>
-
-      <tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/[VERSION]/redhat9</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "GNU/Linux RedHat 9")?></td>
-      </tr>
-
-	<tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/[VERSION]/suse82</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "GNU/Linux SuSE 8.2")?></td>
-      </tr>
-    </table>
-    </dd>
-
-    <dd>
-
-        <?php echo _("Install using the following command:");?>
-          <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
-            <tr><td>
-              rpm -Uvh pgadmin*.rpm
-            </td></tr>
-	  </table>
-
-   </dd>
-  </dl>
-  <a class="topOfPage" href="#top" title="Top Of Page">top</a>
+            <?php echo _("Install using the following command:");?>
+            <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
+                <tr><td>
+                    rpm -Uvh pgadmin*.rpm
+                </td></tr>
+	        </table>
+        </dd>
+    </dl>
+    <a class="topOfPage" href="#top" title="Top Of Page">top</a>
 
 
-  <h1 id="slackware"><?php echo "Slackware packages";?></h1>
-  <dl>
-    <dt><?php echo  sprintf(_("Packager: %s"), "Dave Page");?></dt>
 
-  <dd>
-    <?php echo sprintf(_("pgAdmin III packages are available for %s."), "GNU/Linux Slackware 9.0/9.1");?>
-  </dd>
+    <h1 id="slackware"><?php echo "Slackware packages";?></h1>
+    <dl>
+        <dt><?php echo  sprintf(_("Packager: %s"), "Dave Page");?></dt>
 
-  <dd>
-        <?php echo sprintf(_("Download pgAdmin III package from our <a href='%s'>mirrors</a>."), "#mirrors");?>
-	<?php echo "On each mirror, the packages are located in:";?>
-  </dd>
-    <dd>
+        <dd>
+            <?php echo _("pgAdmin III packages for Slackware 9.0, 9.1 & -current are available from the <a href=\"%s\">PostgreSQL mirror network</a>."), "http://www.postgresql.org/ftp/pgadmin3/release/");?>
+        </dd>
 
-	   <table width= '100%' cellspacing=0 cellpadding=10 border=0>
-      <tr bgcolor=beige>
-        <td><?php echo _("Location")?></td>
-        <td></td>
-        <td><?php echo _("Description")?></td>
-      </tr>
+        <dd>
+            <?php echo _("Install the downloaded package using Slackware pkgtool or installpkg.");?>
+        </dd>
+    </dl>
+    <a class="topOfPage" href="#top" title="Top Of Page">top</a>
 
-      <tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/[VERSION]/slackware91</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "GNU/Linux Slackware 9.1")?></td>
-      </tr>
-      
-      <tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/[VERSION]/slackware9</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "GNU/Linux Slackware 9.0")?></td>
-      </tr>
-      </tr>
-    </table>
-    </dd>
 
-<dd>
-    <?php echo _("Install the downloaded package using Slackware pkgtool or installpkg.");?>
-    </dd>
+    <h1 id="freebsd"><?php echo "FreeBSD packages";?></h1>
+
+    <dl>
+        <dt><?php echo  sprintf(_("Packager: %s"), "斉藤　浩 (Hiroshi Saito)");?></dt>
+        <dd>
+            <?php echo _("pgAdmin III packages for FreeBSD are available from the <a href=\"%s\">PostgreSQL mirror network</a>."), "http://www.postgresql.org/ftp/pgadmin3/release/");?>
+        </dd>
+
+        <dd>
+            <?php echo sprintf(_("Download pgAdmin III package from our <a href='%s'>mirrors</a>."), "#mirrors");?>
+	        <?php echo "On each mirror, the packages are located in:";?>
+        </dd>
+
+        <dd><?php echo _("Install using the following command:");?>
+            <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
+                <tr><td>
+                    pkg_add pgadmin3-yyyymmdd.tgz
+                </td></tr>
+            </table>
+        </dd>
+        
+        <dd>
+            <?php echo _("Create a symlink to pgAdmin III binary.");?>
+            <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
+                <tr><td>
+                    ln -sf /usr/local/pgadmin3/bin/pgadmin3 /usr/local/bin/pgadmin3
+                </td></tr>
+            </table>
+        </dd>
+
+        <dd>
+            <?php echo _("Check that pgAdmin III executable is in the path.");?>
+            <br/>
+            <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
+                <tr><td>
+                    which pgadmin3<br />
+                    # /usr/local/bin/pgadmin3
+                </td></tr>
+            </table>
+        </dd>
 	</dl>
-  <a class="topOfPage" href="#top" title="Top Of Page">top</a>
+    <a class="topOfPage" href="#top" title="Top Of Page">top</a>
 
-  <h1 id="freebsd"><?php echo "FreeBSD packages";?></h1>
+    <h1 id="win32"><?php echo _("MS Windows 2000 & XP");?></h1>
 
-  <dl>
-    <dt><?php echo  sprintf(_("Packager: %s"), "斉藤　浩 (Hiroshi Saito)");?></dt>
-  <dd>
-    <?php echo sprintf(_("pgAdmin III packages are available for %s."), "FreeBSD 5.1");?>
-  </dd>
+    <dl>
+        <dt><?php echo  sprintf(_("Packager: %s"), "Dave Page");?></dt>
 
-  <dd>
-        <?php echo sprintf(_("Download pgAdmin III package from our <a href='%s'>mirrors</a>."), "#mirrors");?>
-	<?php echo "On each mirror, the packages are located in:";?>
-  </dd>
-    <dd>
+        <dd>
+            <?php echo sprintf(_("pgAdmin III packages are available for %s from the <a href=\"%s\">PostgreSQL mirror network</a>."), "MS Windows 2000/XP", "http://www.postgresql.org/ftp/pgadmin3/release/");?>
+            <br><br>
+            <?php echo sprintf(_("Note that the <a href='%s'>pgInstaller distribution of PostgreSQL for Windows</a> comes with pgAdmin."), "http://www.postgresql.org/ftp/win32");?>
+        </dd>
 
-	   <table width= '100%' cellspacing=0 cellpadding=10 border=0>
-      <tr bgcolor=beige>
-        <td><?php echo _("Location")?></td>
-        <td></td>
-        <td><?php echo _("Description")?></td>
-      </tr>
+        <dd>
+            <li><?php echo _("Double-click on the installer and follow the instructions.");?>
+        </dd>
+    </dl>
+    <a class="topOfPage" href="#top" title="Top Of Page">top</a>
 
-      <tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/[VERSION]/freebsd51</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "FreeBSD 5.1")?></td>
-      </tr>
-      </tr>
-    </table>
-    </dd>
 
-     <dd><?php echo _("Install using the following command:");?>
-          <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
-            <tr><td>
-              pkg_add pgadmin3-yyyymmdd.tgz
-            </td></tr>
-          </table>
-     </dd>
-     <dd>
-      <?php echo _("Create a symlink to pgAdmin III binary.");?>
-          <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
-            <tr><td>
-              ln -sf /usr/local/pgadmin3/bin/pgadmin3 /usr/local/bin/pgadmin3
-            </td></tr>
-          </table>
-     </dd>
+    <h1 id="win32"><?php echo _("Mac OSX (Panther/Tiger)");?></h1>
 
-     <dd>
-     <?php echo _("Check that pgAdmin III executable is in the path.");?>
-        <br/>
-          <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
-            <tr><td>
-              which pgadmin3
-        # /usr/local/bin/pgadmin3
-            </td></tr>
-          </table>
-       </dd>
-	</dl>
-  <a class="topOfPage" href="#top" title="Top Of Page">top</a>
+    <dl>
+        <dt><?php echo  sprintf(_("Packager: %s"), "Adam H. Pendleton");?></dt>
 
-  <h1 id="win32"><?php echo _("MS Windows 2000 & XP");?></h1>
+        <dd>
+            <?php echo sprintf(_("pgAdmin III packages are available for %s from the <a href=\"%s\">PostgreSQL mirror network</a>."), "Mac OSX (Panther/Tiger)", "http://www.postgresql.org/ftp/pgadmin3/release/");?>
+        </dd>
 
-  <dl>
-    <dt><?php echo  sprintf(_("Packager: %s"), "Dave Page");?></dt>
-
-    <dd>
-      <?php echo sprintf(_("pgAdmin III packages are available for %s."), "MS Windows 2000/XP");?>
-      <br><br>
-      <?php echo sprintf(_("Note that the <a href='%s'>pgInstaller distribution of PostgreSQL for Windows</a> comes with pgAdmin."), "http://www.postgresql.org/ftp/win32");?>
-    </dd>
-  <dd>
-        <?php echo sprintf(_("Download pgAdmin III package from our <a href='%s'>mirrors</a>."), "#mirrors");?>
-	<?php echo _("On each mirror, the packages are located in:");?>
-  </dd>
-    <dd>
-
-	   <table width= '100%' cellspacing=0 cellpadding=10 border=0>
-      <tr bgcolor=beige>
-        <td><?php echo _("Location")?></td>
-        <td></td>
-        <td><?php echo _("Description")?></td>
-      </tr>
-
-      <tr bgcolor=lightyellow>
-        <td>[MIRROR URL]/pgadmin3/release/win32</td>
-        <td></td>
-        <td><?php echo sprintf(_("pgAdmin III binary packages for %s"), "MS Windows 2000/XP")?></td>
-      </tr>
-      </tr>
-    </table>
-    </dd>
-    <dd>
-        <li><?php echo _("Double-click on the installer and follow the instructions.");?>
-    </dd>
-	</dl>
-  <a class="topOfPage" href="#top" title="Top Of Page">top</a>
-
+        <dd>
+            <li><?php echo _("Double-click on the installer and follow the instructions.");?>
+        </dd>
+    </dl>
+    <a class="topOfPage" href="#top" title="Top Of Page">top</a>
+  
+  
   <h1 id="source"><?php echo _("Installing pgAdmin III from source code")?></h1>
   <dt><?php echo _("Requirements");?></dt>
   <dd>
@@ -412,11 +315,6 @@
       <br/>
       <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
         <tr><td>
-          # Warning: MacOsX port is completely experimental.<br/>
-          # This information is intended for pgAdmin team only.<br/>
-          # Please checkout wxWindows and pgAdmin from CVS/SVN only.<br/>
-          # To coordinate efforts, please contact us on the mailing list<br/>
-          <br/>
           # <?php echo _("Note - release builds should be configured without the --enable-debug option.");?><br/><br/>
 					cd wxWidgets*<br/>
           ../configure --with-mac --disable-shared --enable-debug<br/>
@@ -456,7 +354,8 @@
       <li><?php echo _("Compile pgAdmin III.");?><br/>
         <table width="100%" border="0" cellspacing="1" cellpadding="8" bgcolor="#efefef">
             <tr><td>
-            # <?php echo _("Note - release builds should be configured without the --enable-debug option.");?><br/><br/>
+            # <?php echo _("Note - release builds should be configured without the --enable-debug option.");?><br/>
+            # <?php echo _("Note - When building on Mac OSX, you should also include the --enable-appbundle option.");?><br/><br/>
             ./configure --enable-static --enable-debug<br/>
             make all<br/>
             sudo make install
@@ -486,16 +385,6 @@
     </ul>
     <?php echo sprintf(_("Please refer to our <a href='%s'>installation instructions</a>."), "#binaries");?>
   </dd>
-  <a class="topOfPage" href="#top" title="Top Of Page">top</a>
-
-
-    <h1 id="mirrors"><?php echo _("Download mirrors")?></h1>
-  <dd>
-    <?php echo sprintf(_('pgAdmin III can be downloaded from a <a href="%s">large number of mirrors</a>.'), 'http://www.postgresql.org/ftp/pgadmin3/')?>
-    
-    <?php echo sprintf(_('The mirrors are kindly provided by the <a href="%s">PostgreSQL</a> project.'), 'http://www.postgresql.org/')?><br>
-  </dd>
-
   <a class="topOfPage" href="#top" title="Top Of Page">top</a>
 
   <h1 id="languages"><?php echo _("Additional languages");?></h1>

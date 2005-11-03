1cb5276537b704fc759cf7766f1b1e60c7545bfc
Dave Page <dpage@pgadmin.org>
2005-11-03 08:35:11 +0000

    Update translation list now fr_FR is up to date.
    git-svn-id: svn://svn.pgadmin.org/trunk@4643 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/cnt/cnt_translation_status.php
+++ pgadmin3/cnt/cnt_translation_status.php
@@ -25,7 +25,7 @@
   $translated->addLanguage ("es_ES", "Spanish", "Diego A. Gil", "diego@adminsa.com", "pgadmin3.po","i18n/es_ES/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $outofdate->addLanguage ("fa_IR", "Farsi", "Abbas Izad", "abbasizad@hotmail.com", "pgadmin3.po", "i18n/fa_IR/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $translated->addLanguage ("fi_FI", "Finnish", "Jori Luoto", "psql@jorch.net", "pgadmin3.po","i18n/fi_FI/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
-  $outofdate->addLanguage ("fr_FR", "French", "Raphaël Enrici", "blacknoz@club-internet.fr",  "pgadmin3.po","i18n/fr_FR/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
+  $translated->addLanguage ("fr_FR", "French", "Guillaume LELARGE", "gleu@wanadoo.fr",  "pgadmin3.po","i18n/fr_FR/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $outofdate->addLanguage ("gl_ES", "Galician", "Antonio Ricoy", "aricoy@edu.xunta.es", "pgadmin3.po","i18n/gl_ES/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $outofdate->addLanguage ("gu_IN", "Gujarati", "Unmaintained", "pgadmin-hackers@postgresql.org", "pgadmin3.po", "i18n/gu_IN/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $outofdate->addLanguage ("hi_IN", "Hindi", "Unmaintained", "pgadmin-hackers@postgresql.org", "pgadmin3.po", "i18n/hi_IN/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");

d356800429203e690387a96161b5b57c88947742
Dave Page <dpage@pgadmin.org>
2006-01-17 13:15:15 +0000

    Remove the old website from SVN.
    git-svn-id: svn://svn.pgadmin.org/trunk@4906 a7884b65-44f6-0310-8a51-81a127f17b15

--- pgadmin3/tpl/include.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$_base = dirname($_SERVER["SCRIPT_FILENAME"]);
-	include_once ($_base."/class/pgadmin_session.php");
-	include_once ($_base."/class/pgadmin_po.php");
-	include_once ($_base."/class/pgadmin_thumb.php");
-
-  $session = new pgadmin_session();
-?>

b8dd609ad399859f67f1dd066f76471ba2992f5d
Dave Page <dpage@pgadmin.org>
2006-01-17 14:54:48 +0000

    Use full paths to the doc indexes to avoid so Apache rewrite oddities on Ferengi
    git-svn-id: svn://svn.pgadmin.org/trunk@4914 a7884b65-44f6-0310-8a51-81a127f17b15

--- docs/menu.php
+++ docs/menu.php
@@ -4,8 +4,8 @@ function www_menu()
 {
   $output = '
 <div' . www_current_menu_page("index.php") . '><a href="index.php">&rsaquo; ' . _("Introduction") . '</a></div>
-<div' . www_current_menu_page("dev/index.php") . '><a href="dev/">&rsaquo; ' . sprintf(_("Development %s"), "1.5") . '</a></div>
-<div' . www_current_menu_page("1.4/index.php") . '><a href="1.4/">&rsaquo; ' . sprintf(_("Release %s"), "1.4") . '</a></div>
+<div' . www_current_menu_page("dev/index.html") . '><a href="dev/index.html">&rsaquo; ' . sprintf(_("Development %s"), "1.5") . '</a></div>
+<div' . www_current_menu_page("1.4/index.html") . '><a href="1.4/index.html">&rsaquo; ' . sprintf(_("Release %s"), "1.4") . '</a></div>
 ';
 
   return $output;

1f3723740b5e20d4f36e797c9e507b4dbdd1f6f3
Dave Page <dpage@pgadmin.org>
2006-01-18 08:30:02 +0000

    Add an OSX screenshot, and reorder them on the page.
    git-svn-id: svn://svn.pgadmin.org/trunk@4926 a7884b65-44f6-0310-8a51-81a127f17b15

--- screenshots/index.php
+++ screenshots/index.php
@@ -11,19 +11,23 @@ $PAGE_CONTENT = '
 
 <p>' . sprintf(_("To view a full size version of any screenshot, simply click on it. In addition to the those found in this section additional images can also be found in the <a href=\"%s\">online documentation</a>."), "/docs/") . '</p>
 
-<p>' . _("We provide source and binary packages for Linux, FreeBSD, Mac OSX and Microsoft Windows &trade;. Solaris binaries may also be compiled from the source code.") . '</p>
-
-<h2>' . _("Linux") . '</h2>
-<center><a href="/images/screenshots/pgadmin3_linux.png" target="_Shot"><img src="/images/screenshots/pgadmin3_linux.png" alt="' . _("pgAdmin III running on Linux") . '" width="200" height="213" /></a></center>
+<p>' . _("We provide source and binary packages for FreeBSD, Linux, Mac OSX and Microsoft Windows &trade;. Solaris binaries may also be compiled from the source code.") . '</p>
 
 <h2>' . _("FreeBSD") . '</h2>
 <center><a href="/images/screenshots/pgadmin3_freebsd.png" target="_Shot"><img src="/images/screenshots/pgadmin3_freebsd.png" alt="' . _("pgAdmin III running on FreeBSD") . '" width="254" height="170" /></a></center>
 
-<h2>' . _("Windows") . '</h2>
-<center><a href="/images/screenshots/pgadmin3_win32.png" target="_Shot"><img src="/images/screenshots/pgadmin3_win32.png" alt="' . _("pgAdmin III running on Windows") . '" width="253" height="208" /></a></center>
+<h2>' . _("Linux") . '</h2>
+<center><a href="/images/screenshots/pgadmin3_linux.png" target="_Shot"><img src="/images/screenshots/pgadmin3_linux.png" alt="' . _("pgAdmin III running on Linux") . '" width="200" height="213" /></a></center>
+
+<h2>' . _("Mac OSX") . '</h2>
+<center><a href="/images/screenshots/pgadmin3_macosx.png" target="_Shot"><img src="/images/screenshots/pgadmin3_macosx.png" alt="' . _("pgAdmin III running on Mac OSX") . '" width="256" height="192" /></a></center>
 
 <h2>' . _("Solaris") . '</h2>
 <center><a href="/images/screenshots/pgadmin3_solaris.png" target="_Shot"><img src="/images/screenshots/pgadmin3_solaris.png" alt="' . _("pgAdmin III running on Solaris") . '" width="200" height="150" /></a></center>
+
+<h2>' . _("Windows &trade;") . '</h2>
+<center><a href="/images/screenshots/pgadmin3_win32.png" target="_Shot"><img src="/images/screenshots/pgadmin3_win32.png" alt="' . _("pgAdmin III running on Windows") . '" width="253" height="208" /></a></center>
+
 '; 
 
 // Display the page

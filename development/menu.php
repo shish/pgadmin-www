324fb0dde5d2522321fa631ededefda1ca8a89c1
Dave Page <dpage@pgadmin.org>
2006-01-20 09:28:34 +0000

    Add the SVN browser to the menu for convenience
    git-svn-id: svn://svn.pgadmin.org/trunk@4939 a7884b65-44f6-0310-8a51-81a127f17b15

--- development/menu.php
+++ development/menu.php
@@ -5,6 +5,7 @@ function www_menu()
   $output = '
 <div' . www_current_menu_page("index.php") . '><a href="index.php">&rsaquo; ' . _("Introduction") . '</a></div>
 <div' . www_current_menu_page("svn.php") . '><a href="svn.php">&rsaquo; ' . _("SVN access") . '</a></div>
+<div' . www_current_menu_page("http://svn.pgadmin.org/") . '><a href="http://svn.pgadmin.org/">&rsaquo; ' . _("SVN browser") . '</a></div>
 <div' . www_current_menu_page("list.php") . '><a href="list.php">&rsaquo; ' . _("Mailing list") . '</a></div>
 <div' . www_current_menu_page("team.php") . '><a href="team.php">&rsaquo; ' . _("Team") . '</a></div>
 <div' . www_current_menu_page("todo.php") . '><a href="todo.php">&rsaquo; ' . _("TODO list") . '</a></div>

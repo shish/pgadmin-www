05a6e2482aaa6695162bf17e935dd7c6f03ba608
Dave Page <dpage@pgadmin.org>
2006-05-04 20:54:59 +0000

    Promote zh_TW
    git-svn-id: svn://svn.pgadmin.org/trunk@5122 a7884b65-44f6-0310-8a51-81a127f17b15

--- translation/config.php
+++ translation/config.php
@@ -57,7 +57,7 @@
   $outofdate->addLanguage ("ur_PK", "Urdu", "Unmaintained", "pgadmin-hackers@postgresql.org", "pgadmin3.po","i18n/ur_PK/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $outofdate->addLanguage ("vi_VN", "Vietnamese", "Unmaintained", "pgadmin-hackers@postgresql.org", "pgadmin3.po", "i18n/vi_VN/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
   $outofdate->addLanguage ("zh_CN", "Chinese simplified", "Unmaintained", "pgadmin-hackers@postgresql.org", "pgadmin3.po", "i18n/zh_CN/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
-  $outofdate->addLanguage ("zh_TW", "Chinese traditional", "Kuo ChaoYi", "kuo.chaoyi@gmail.com", "pgadmin3.po", "i18n/zh_TW/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
+  $translated->addLanguage ("zh_TW", "Chinese traditional", "Kuo ChaoYi", "kuo.chaoyi@gmail.com", "pgadmin3.po", "i18n/zh_TW/pgadmin3.po", "pgadmin3.pot", "pgadmin3.pot");
 
 
 ?>

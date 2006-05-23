2b7f97e3b2a63212221f4d998a1d29468ba4fd8d
Dave Page <dpage@pgadmin.org>
2006-05-23 09:44:24 +0000

    Update build instructions
    git-svn-id: svn://svn.pgadmin.org/trunk@5184 a7884b65-44f6-0310-8a51-81a127f17b15

--- download/source.php
+++ download/source.php
@@ -14,7 +14,7 @@ $PAGE_CONTENT = '
     <ul>
       <li>' . _("Linux, FreeBSD and other *nixes: Recent versions of GTK2.2, automake, autoconf and gcc are needed. The best solution is to use a recent distribution.") . '</li>
 
-      <li>' . _("Windows 2000, XP, 2003: Microsoft Visual C++ 6.0 or newer.") . '</li>
+      <li>' . _("Windows 2000, XP, 2003: Microsoft Visual C++ 2005.") . '</li>
 
       <li>' . _("Mac OSX: XCode 2.0 or above. Recent versions of autoconf and automake may also be required if building from SVN.") . '</li>
     </ul>
@@ -22,16 +22,16 @@ $PAGE_CONTENT = '
 
   <li>' . sprintf(_("<a href=\"%s\">wxWidgets 2.6.x</a>. Currently, this is the only version we support."), "http://www.wxwidgets.org") . '<br />&nbsp;</li>
 
-  <li>' . sprintf(_("<a href=\"%s\">libxml2 2.5.x</a> or above."), "http://www.xmlsoft.org") . '<br />&nbsp;</li>
+  <li>' . sprintf(_("<a href=\"%s\">libxml2 2.5.x</a> or above (for pgAdmin 1.5+)."), "http://www.xmlsoft.org") . '<br />&nbsp;</li>
   
-  <li>' . sprintf(_("<a href=\"%s\">libxslt 1.1.x</a> or above."), "http://www.xmlsoft.org") . '<br />&nbsp;</li>
+  <li>' . sprintf(_("<a href=\"%s\">libxslt 1.1.x</a> or above (for pgAdmin 1.5+)."), "http://www.xmlsoft.org") . '<br />&nbsp;</li>
 
-  <li>' . sprintf(_("<a href=\"%s\">PostgreSQL 7.3</a> or above."), "http://www.postgresql.org/ftp/latest/") . '<br />&nbsp;</li>
+  <li>' . sprintf(_("<a href=\"%s\">PostgreSQL 7.3</a> or above (8.0 or above on Windows)."), "http://www.postgresql.org/ftp/latest/") . '<br />&nbsp;</li>
 </ul>
 
 <p>' . sprintf(_("Precompiled XML/XSLT and dependency packages for Windows can be found at <a href=\"%s\">zlatkovic.com</a>. You probably need iconv 1.9.x and zlib 1.2.x as well as libxml2 and libxslt."), "http://www.zlatkovic.com/pub/libxml/") . '</p>
 
-<h3>' . _("Installing wxWidgets") . '</h3>
+<h3>' . _("Building wxWidgets") . '</h3>
 
 <p>' . _("Unpack the wxWidgets tarball or zip file and follow the appropriate set of instruction below.") . '</p>
 
@@ -60,6 +60,8 @@ sudo make install
 
 <p>' . _("A Visual Studio 6.0 project workspace file is in the <i>xtra/wx-build</i> directory of the pgAdmin source tarball, along with a suitable <i>setup.h</i> file. Copy these to the appropriate <i>build/</i> and <i>include/wx/</i> directories in your wxWidgets source tree, and open the workspace in Visual Studio. Use the batch build facility to build all the wxWidgets libraries. To save time, you can omit all \"Universal\" and \"DLL\" builds.") . '</p>
 
+<p>' . sprintf(_("Note that as of mid May 2006, the development environment for pgAdmin 1.5+ was upgraded to Visual C++ 2005. You must compile wxWidgets with this version of Visual C++ as well. You can download the Visual Studio Express editions free of charge from <a href=\"%s\">Microsoft</a>&trade;."), "http://msdn.microsoft.com/vstudio/express/default.aspx") . '</p>
+
 <h4>' . _("Mac OSX") . '</h4>
 
 <p>' . _("Note:") . '</p>
@@ -80,7 +82,7 @@ make<br />
 sudo make install
 </div>
 
-<h3>' . _("Installing pgAdmin III") . '</h3>
+<h3>' . _("Building pgAdmin III") . '</h3>
 
 <h4>' . _("Linux, FreeBSD and Solaris") . '</h4>
 
@@ -105,7 +107,17 @@ sudo make install<br />
 
 <h4>' . _("Microsoft Windows 2000, XP & 2003") . '</h4>
 
-<p>' . _("A Visual Studio project file for pgAdmin may be found in the <i>src/</i> subdirectory of the pgAdmin source tree. Load the project into Visual Studio and build the desired targets.") . '</p>
+<p>' . _("The pgAdmin dependencies must first be installed in the appropriate locations on your system:") . '</p>
+<ol>
+  <li>' . _("PostgreSQL 8.1, installed into the default location from the official PostgreSQL installer (pgInstaller).") . '<br />&nbsp;</li>
+  <li>' . _("wxWidgets 2.6, installed into \$BUILD_ROOT/pgadmin3-deps/wxwidgets.") . '<br />&nbsp;</li>
+  <li>' . _("libxml2 2.5, installed into \$BUILD_ROOT/pgadmin3-deps/libxml2.") . '<br />&nbsp;</li>
+  <li>' . _("libxslt 1.1, installed into \$BUILD_ROOT/pgadmin3-deps/libxslt.") . '</li>
+</ol>
+
+<p>' . _("In addition, libxml2/libxslt may also require iconv and zlib. If you are building the pgAdmin installer, these must be installed into \$BUILD_ROOT/pgadmin3-deps/iconv and \$BUILD_ROOT/pgadmin3-deps/zlib respectively.") . '</p>
+
+<p>' . _("The source tree should be unpacked into \$BUILD_ROOT. The directory can be called whatever you like, as long as its at the right directory level - for example, you might have \$BUILD_ROOT/pgadmin3-deps, \$BUILD_ROOT/pgadmin3-dev and \$BUILD_ROOT/pgadmin3-1.4. A Visual Studio/C++ 2005 solution file for pgAdmin may be found in the root of the source tree. Load the project into Visual C++ and build the desired targets.") . '</p>
 
 <h4>' . _("Mac OSX") . '</h4>
 

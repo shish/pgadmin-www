610922cc0a4ad216f55a611b036d9649787721a4
Dave Page <dpage@pgadmin.org>
2006-08-30 16:14:14 +0000

    Add Hiroshi's Suse 10 XGL screenshot
    git-svn-id: svn://svn.pgadmin.org/trunk@5311 a7884b65-44f6-0310-8a51-81a127f17b15

---
+++ screenshots/xgl.php
@@ -0,0 +1,19 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . "/include/page.php"; 
+
+$PAGE_TITLE = _("pgAdmin III: Screenshots - Suse 10 XGL");
+$PAGE_CONTENT = '
+
+<h1>' . _("Suse 10 XGL") . '</h1>
+
+<p>' . _("pgAdmin III even works in 3D!!.") . '</p>
+
+<center><a href="/images/screenshots/pgadmin3_suse10_xgl.png" target="_Shot"><img src="/images/screenshots/pgadmin3_suse10_xgl.png" alt="' . _("pgAdmin running on Suse 10 XGL") . '" width="195" height="131" /></a></center>
+
+';
+
+// Display the page
+www_page($PAGE_TITLE, $PAGE_CONTENT);
+
+?>

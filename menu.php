d9767c646dd9918786f2ff863e359dae6040e97c
Dave Page <dpage@pgadmin.org>
2007-02-14 09:40:11 +0000

    Add privacy policy.
    git-svn-id: svn://svn.pgadmin.org/trunk@5915 a7884b65-44f6-0310-8a51-81a127f17b15

--- menu.php
+++ menu.php
@@ -8,6 +8,7 @@ function www_menu()
 <div' . www_current_menu_page("features.php") . '><a href="features.php">&rsaquo; ' . _("Features") . '</a></div>
 <div' . www_current_menu_page("contribute.php") . '><a href="contribute.php">&rsaquo; ' . _("Contribute") . '</a></div>
 <div' . www_current_menu_page("licence.php") . '><a href="licence.php">&rsaquo; ' . _("Licence") . '</a></div>
+<div' . www_current_menu_page("privacypolicy.php") . '><a href="privacypolicy.php">&rsaquo; ' . _("Privacy policy") . '</a></div>
 ';
 
   return $output;

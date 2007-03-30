cc50be66f82f57bf32376aa442b081f62b140eae
Hiroshi Saito <z-saito@guitar.ocn.ne.jp>
2007-03-30 17:51:27 +0000

    Sorry, renewal was overdue.
    git-svn-id: svn://svn.pgadmin.org/trunk@6146 a7884b65-44f6-0310-8a51-81a127f17b15

--- download/opensuse.php
+++ download/opensuse.php
@@ -11,6 +11,7 @@ $PAGE_CONTENT = '
 
 <p>' . _("OpenSUSE packages built on a server periodically updated to -current are available for download. These builds should run on most recent versions of OpenSUSE.") . '</p>
 <ul>
+  <li>' . sprintf(_("<a href=\"%s\">pgAdmin v%s</a>"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.6.3/opensuse/", "1.6.3") . '<br />&nbsp;</li>
   <li>' . sprintf(_("<a href=\"%s\">pgAdmin v%s</a>"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.6.2/opensuse/", "1.6.2") . '<br />&nbsp;</li>
 </ul>
 

8aa8013d22d7403a20444d7470289ca492b97d10
Dave Page <dpage@pgadmin.org>
2007-04-30 12:12:50 +0000

    Road map item complete.
    git-svn-id: svn://svn.pgadmin.org/trunk@6241 a7884b65-44f6-0310-8a51-81a127f17b15

--- development/roadmap.php
+++ development/roadmap.php
@@ -59,7 +59,7 @@ $PAGE_CONTENT = '
       <li><del>' . _("Add a 'Run now' context menu option for Jobs.") . '</del><br />&nbsp;</li>
       <li><del>' . _("Retain column sizes in the Query Tool results grid when a query is re-run.") . '</del><br />&nbsp;</li>
       <li><del>' . _("Ensure all file open/save dialogues remember their last location.") . '</del><br />&nbsp;</li>
-      <li>' . _("Add line-ending conversion options to the query tool.") . '<br />&nbsp;</li>
+      <li><del>' . _("Add line-ending conversion options to the query tool.") . '</del><br />&nbsp;</li>
     </ul>
   </li>
 

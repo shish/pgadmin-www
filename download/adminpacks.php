ae89dbffacce92553ef72c25609fc3efb6710bfb
Dave Page <dpage@pgadmin.org>
2007-10-21 10:48:09 +0000

    Don't need so many adminpacks
    git-svn-id: svn://svn.pgadmin.org/trunk@6780 a7884b65-44f6-0310-8a51-81a127f17b15

--- download/adminpacks.php
+++ download/adminpacks.php
@@ -9,14 +9,8 @@ $PAGE_CONTENT = '
 
 <p>' . _("The admin packs to enable server instrumentation on PostgreSQL 8.x servers can be found in the <i>adminpacks</i> directory on the mirror network.") . '</p>
 <ul>
-  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.6.3/adminpacks/", "1.6.3") . '<br />&nbsp;</li>
-  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.6.2/adminpacks/", "1.6.2") . '<br />&nbsp;</li>
-  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.6.1/adminpacks/", "1.6.1") . '<br />&nbsp;</li>
-  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.6.0/adminpacks/", "1.6.0") . '<br />&nbsp;</li>
-  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.4.3/adminpacks/", "1.4.3") . '<br />&nbsp;</li>
-  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.4.2/adminpacks/", "1.4.2") . '<br />&nbsp;</li>
-  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.4.1/adminpacks/", "1.4.1") . '<br />&nbsp;</li>
-  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.4.0/adminpacks/", "1.4.0") . '<br />&nbsp;</li>
+  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s and above"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.6.3/adminpacks/", "1.6.x") . '<br />&nbsp;</li>
+  <li>' . sprintf(_("<a href=\"%s\">Admin pack</a> for pgAdmin v%s"), "http://www.postgresql.org/ftp/pgadmin3/release/v1.4.3/adminpacks/", "1.4.x") . '<br />&nbsp;</li>
 </ul>
 
 <p>' . _("Install the appropriate admin pack for the version of pgAdmin that you use, and the server you are running. pgInstaller distributions of PostgreSQL for Windows include the admin packs.") . '</p>

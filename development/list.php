c94c51099c0a111aafc57f6ea22e53408511b716
Guillaume Lelarge <guillaume@lelarge.info>
2008-11-04 10:02:02 +0000

    Mailing lists archives are only available from other websites, ie postgresql.org and mail-archive.com.
    git-svn-id: svn://svn.pgadmin.org/trunk@7495 a7884b65-44f6-0310-8a51-81a127f17b15

--- development/list.php
+++ development/list.php
@@ -9,7 +9,7 @@ $PAGE_CONTENT = '
 
 <p>' . sprintf(_("The pgadmin-hackers mailing list is intended for developers who wish to contribute to pgAdmin project. You are more than welcome to subscribe, but please remember to use <a href=\"%s\">pgadmin-support</a> list for end user support issues. To subscribe to the mailing list, send an email to <a href=\"%s\">%s</a> with the word 'subscribe', without quotes, as the body of message. To unsubscribe, send the word 'unsubscribe'."), "../support/list.php", "mailto:pgadmin-hackers-request@postgresql.org", "pgadmin-hackers-request@postgresql.org") . '</p>
 
-<p>' . sprintf(_("Messages sent to the list may be browsed <a href=\"%s\">here</a> and are archived at <a href=\"%s\">PostgreSQL</a> and  <a href=\"%s\">mail-archive.com</a>."), "/archives/pgadmin-hackers/index.php", "http://archives.postgresql.org/pgadmin-hackers/", "http://www.mail-archive.com/pgadmin-hackers@postgresql.org") . '</p>
+<p>' . sprintf(_("Messages sent to the list are archived at <a href=\"%s\">PostgreSQL</a> and  <a href=\"%s\">mail-archive.com</a>."), "http://archives.postgresql.org/pgadmin-hackers/", "http://www.mail-archive.com/pgadmin-hackers@postgresql.org") . '</p>
 
 '; 
 

1ce8a3dd72ef960d9a3e6b725964b7411ced0ef2
Dave Page <dpage@pgadmin.org>
2009-03-13 16:07:32 +0000

    1.10.0 Beta 1 announcement
    git-svn-id: svn://svn.pgadmin.org/trunk@7695 a7884b65-44f6-0310-8a51-81a127f17b15

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ $PAGE_CONTENT = '
 
 <h1>' . _("Latest news") . '</h1>
 
+<h2>' . sprintf(_("%s - pgAdmin III v%s released"), "2009-03-13", "1.10.0 BETA 1") . '</h2>
+
+<p>' . _("pgAdmin 1.10.0 BETA 1 has now been released! Highlights of the new release include a graphical query builder, a scripting engine in the query tool, and support for PostgreSQL 8.4 in addition to <a href=\"/development/changelog.php\">numerous other changes</a>. Please visit the <a href=\"http://www.postgresql.org/ftp/pgadmin3/release/v1.10.0-beta1/\">PostgreSQL download site</a> to get a copy and help with the testing. Any questions or other support issues should be sent to <a href=\"mailto:pgadmin-support@postgresql.org\">pgadmin-support@postgresql.org</a>.") . '</a></p>
+
 <h2>' . sprintf(_("%s - pgAdmin III v%s released"), "2008-06-05", "1.8.4") . '</h2>
 
 <p>' . _("pgAdmin 1.8.4 has now been released! Please see the <a href=\"news.php\">news page</a> for further details, and <a href=\"/download/\">the download page</a> to get your copy. Any questions or other support issues should be sent to <a href=\"mailto:pgadmin-support@postgresql.org\">pgadmin-support@postgresql.org</a>.") . '</a></p>

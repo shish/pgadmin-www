b8561641e39bc932ad79b3480a369ebd8cb6952f
Guillaume Lelarge <guillaume@lelarge.info>
2010-03-10 19:05:06 +0000

    Fix a sentence.
    git-svn-id: svn://svn.pgadmin.org/trunk@8223 a7884b65-44f6-0310-8a51-81a127f17b15

--- advocacy/index.php
+++ advocacy/index.php
@@ -9,8 +9,7 @@ $PAGE_CONTENT = '
 
 <h2>' . _("Why promote pgAdmin?") . '</h2>
 
-<p>' . _("Promotion is essential for the life and development of the pgAdmin project. As the project gains more and more users it attracts additional developers who contribute new features and bug fixes and helping improve pgAdmin to the benefit of all it's users. As the project has grown even more popular, it has attracted the attention of PostgreSQL companies in addition to the users. Some of these companies have invested in the development of pgAdmin - for example, a Japanese company called <a href="http://www.skcapi.co.jp/">SKC</a> sponsored the development of the Slony-I management features, whilst <a href="http://www.enterprisedb.com">EnterpriseDB</a> provided an Apple Powerbook to allow the Mac OSX port to be properly developed and maintained.") . '</p>
-
+<p>' . _("Promotion is essential for the life and development of the pgAdmin project. As the project gains more and more users it attracts additional developers who contribute new features and bug fixes and helping improve pgAdmin to the benefit of all it's users. As the project has grown even more popular, it has attracted the attention of PostgreSQL companies in addition to the users. Some of these companies have invested in the development of pgAdmin - for example, a Japanese company called <a href=\"http://www.skcapi.co.jp/\">SKC</a> sponsored the development of the Slony-I management features, whilst <a href=\"http://www.enterprisedb.com\">EnterpriseDB</a> provided an Apple Powerbook to allow the Mac OSX port to be properly developed and maintained.") . '</p> 
 <p>' . sprintf(_("It's easy to see how promoting pgAdmin benefits us all in the long term. Read the <a href=\"%s\">strategy</a> page to see how you might be able to help."), "strategy.php") . '</p>
 
 '; 

c8b141c05ae0e425d0ddc4a3d5abbb7a4c6d6c29
Guillaume Lelarge <guillaume@lelarge.info>
2010-04-29 15:33:34 +0000

    Update Debian's notes for download.
    git-svn-id: svn://svn.pgadmin.org/trunk@8308 a7884b65-44f6-0310-8a51-81a127f17b15

--- download/debian.php
+++ download/debian.php
@@ -7,56 +7,25 @@ $PAGE_CONTENT = '
 
 <h1>' . _("Debian") . '</h1>
 
-<h4>' . sprintf(_("Maintainer: %s"), "Rapha&euml;l Enrici") . '</h4>
+<h4>' . sprintf(_("Maintainer: %s"), "Gerfried Fuchs") . '</h4>
 
-<p>' . sprintf(_("pgAdmin packages are available for <a href=\"%s\">Debian</a> stable, testing and unstable distributions. These packages are also well-suited for <a href=\"%s\">Knoppix</a> and other Debian based distributions"), "http://www.debian.org/", "http://www.knoppix.net/") . '</p>
+<p>' . sprintf(_("pgAdmin packages are available for <a href=\"%s\">Debian</a> stable, testing and unstable distributions. These packages are also well-suited for <a href=\"%s\">Ubuntu</a> and other Debian based distributions"), "http://www.debian.org/", "http://www.ubuntu.com/") . '</p>
 
-<h4>' . _("Debian Woody") . '</h4>
+<h4>' . _("Debian Etch (oldstable)") . '</h4>
 
-<p>' . _("We don't support this Debian version anymore.") . '</p>
+<p>' . sprintf(_("pgAdmin %s is <a href=\"%s\">available</a> directly from Debian. You can install pgAdmin just as you would do with any other Debian package (apt-get install pgadmin3)."), "http://packages.debian.org/etch/pgadmin3", "1.4.3") . '</p>
 
-<h4>' . _("Debian Sarge (oldstable)") . '</h4>
+<h4>' . _("Debian Lenny (stable)") . '</h4>
 
-<p>' . _("We don't support this Debian version anymore.") . '</p>
+<p>' . sprintf(_("pgAdmin %s is <a href=\"%s\">available</a> directly from Debian. You can install pgAdmin just as you would do with any other Debian package (apt-get install pgadmin3)."), "http://packages.debian.org/lenny/pgadmin3", "1.8.4") . '</p>
 
-<h4>' . _("Debian Etch (stable)") . '</h4>
+<h4>' . _("Debian Squeeze (testing)") . '</h4>
 
-<p>' . _("pgAdmin 1.4.3 is available directly from Debian. You can install pgAdmin just as you would do with any other Debian package (apt-get install pgadmin3).") . '</p>
-
-<p>' . sprintf(_("If you need a more up to date version of pgAdmin, an APT repository is provided to ease installation. Before you start installation, please choose a mirror from the list of <a href=\"%s\">PostgreSQL mirrors</a>. Click one of the flags, and copy the URL you are redirected to. On each mirror, the packages are located in: <i>/pgadmin3/release/debian</i>"), "http://wwwmaster.postgresql.org/download/mirrors-ftp") . '</p>
-
-<p>' . _("Edit /etc/apt/sources.list file and add the following line:") . '</p>
-
-<div class="pgaCode">
-deb [MIRROR URL]/pgadmin3/release/debian etch pgadmin
-</div>
-
-<p>' . _("For example, for France, you can try:") . '</p>
-
-<div class="pgaCode">
-deb ftp://ftp2.fr.postgresql.org/postgresql/pgadmin3/release/debian etch pgadmin
-</div>
-
-<h4>' . _("Debian Lenny (testing)") . '</h4>
-
-<p>' . sprintf(_("An APT repository is provided to ease installation. Before you start installation, please choose a mirror from the list of <a href=\"%s\">PostgreSQL mirrors</a>. Click one of the
-flags, and copy the URL you are redirected to. On each mirror, the packages are located in: <i>[MIRROR URL]/pgadmin3/release/debian</i>"), "http://wwwmaster.postgresql.org/download/mirrors-ftp") . '</p>
-
-<p>' . _("Edit /etc/apt/sources.list file and add the following line:") . '</p>
-
-<div class="pgaCode">
-deb [MIRROR URL]/pgadmin3/release/debian lenny pgadmin
-</div>
-
-<p>' . _("For example, for France, you can try:") . '</p>
-
-<div class="pgaCode">
-deb ftp://ftp2.fr.postgresql.org/postgresql/pgadmin3/release/debian lenny pgadmin
-</div>
+<p>' . sprintf(_("pgAdmin %s is <a href=\"%s\">available</a> directly from Debian. You can install pgAdmin just as you would do with any other Debian package (apt-get install pgadmin3)."), "http://packages.debian.org/squeeze/pgadmin3", "1.10.2") . '</p>
 
 <h4>' . _("Debian Sid (unstable)") . '</h4>
 
-<p>' . _("pgAdmin is published on the official Debian Unstable repository. However, from time to time unstable may become broken, you can try to check for packages from our mirrors as described above for Debian/testing. You only need to change testing to unstable in the sources.list lines.") . '</p>
+<p>' . sprintf(_("pgAdmin %s is <a href=\"%s\">available</a> directly from Debian. You can install pgAdmin just as you would do with any other Debian package (apt-get install pgadmin3)."), "http://packages.debian.org/sid/pgadmin3", "1.10.2") . '</p>
 
 <h4>' . _("Installation") . '</h4>
 

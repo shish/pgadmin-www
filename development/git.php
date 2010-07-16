aa74eaed00b3f25cfeb469f0ef0006f29dc34362
Dave Page <dpage@pgadmin.org>
2010-07-17 00:44:55 +0100

    Change SVN info to GIT.

---
+++ development/git.php
@@ -0,0 +1,26 @@
+<?php 
+
+include $_SERVER['DOCUMENT_ROOT'] . "/include/page.php"; 
+
+$PAGE_TITLE = _("pgAdmin: Development - GIT access");
+$PAGE_CONTENT = '
+
+<h1>' . _("SVN access") . '</h1>
+
+<p>' . sprintf(_("pgAdmin is available subject to the terms of the <a href=\"%s\">licence</a>. pgAdmin is developed using a number of GIT repositories to hold the source code. Access to the repository is available through a <a href=\"%s\">web interface</a> or using your favourite GIT client. Under Windows, we recommend using <a href=\"%s\">TortoiseGIT</a>, which is free software."), "../licence.php", "http://git.postgresql.org", "http://code.google.com/p/tortoisegit/") . '</p>
+
+<p>' . sprintf(_("To checkout the source from the command line and get started, you might use commands such as shown below on a Linux or Unix system. For further details, please see the <a href=\"%s\">source code</a> build instructions."), "../download/source.php") . '</p>
+
+<div class="pgaCode">
+git clone http://git.postgresql.org/git/pgadmin3.git<br />
+cd pgadmin3<br />
+bash bootstrap<br />
+./configure<br />
+</div>
+
+'; 
+
+// Display the page
+www_page($PAGE_TITLE, $PAGE_CONTENT);
+
+?>
--- development/menu.php
+++ development/menu.php
@@ -4,8 +4,8 @@ function www_menu()
 {
   $output = '
 <div' . www_current_menu_page("index.php") . '><a href="index.php">&rsaquo; ' . _("Introduction") . '</a></div>
-<div' . www_current_menu_page("svn.php") . '><a href="svn.php">&rsaquo; ' . _("SVN access") . '</a></div>
-<div' . www_current_menu_page("http://svn.pgadmin.org/") . '><a href="http://svn.pgadmin.org/">&rsaquo; ' . _("SVN browser") . '</a></div>
+<div' . www_current_menu_page("git.php") . '><a href="git.php">&rsaquo; ' . _("GIT access") . '</a></div>
+<div' . www_current_menu_page("http://git.postgresql.org/") . '><a href="http://git.postgresql.org/">&rsaquo; ' . _("GIT browser") . '</a></div>
 <div' . www_current_menu_page("list.php") . '><a href="list.php">&rsaquo; ' . _("Mailing list") . '</a></div>
 <div' . www_current_menu_page("http://archives.postgresql.org/pgadmin-hackers/") . '><a href="http://archives.postgresql.org/pgadmin-hackers/">&rsaquo; ' . _("List archives") . '</a></div>
 <div' . www_current_menu_page("team.php") . '><a href="team.php">&rsaquo; ' . _("Team") . '</a></div>

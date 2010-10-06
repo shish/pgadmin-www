cd6ac96bdb9da9beb601a910d613b8e5609243ea
Dave Page <dpage@pgadmin.org>
2010-10-06 16:12:47 +0100

    Update for 1.12.1

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ $PAGE_CONTENT = '
 
 <h1>' . _("Latest news") . '</h1>
 
+<h2>' . sprintf(_("%s - pgAdmin v%s released"), "2010-10-05", "1.12.1") . '</h2>
+
+<p>' . _("pgAdmin 1.12.1 has been released, offering a number of bug fixes over the previous version. Please visit the <a href=\"download\">download area</a> to get your copy, or alternatively download a <a href=\"http://www.enterprisedb.com/products/pgdownload.do\">PostgreSQL 9.0.1 Installer</a> which includes pgAdmin.") . '</p>
+
 <h2>' . sprintf(_("%s - pgAdmin v%s released"), "2010-09-20", "1.12.0") . '</h2>
 
 <p>' . sprintf(_("pgAdmin %s has now been released. This release is the culmination of over 12 months of development work. A visual tour of the new release is available <a href=\"/visualtour12.php\">here</a>, or a complete list of changes can be seen in the <a href=\"/development/changelog.php\">CHANGELOG</a>. Please visit the <a href=\"download\">download area</a> to get your copy, or alternatively download a <a href=\"http://www.enterprisedb.com/products/pgdownload.do\">PostgreSQL 9.0 Installer</a> which includes pgAdmin."), "1.12.0") . '</p>
@@ -24,7 +28,7 @@ $PAGE_CONTENT = '
 <p>' . sprintf(_("pgAdmin is developed by a community of PostgreSQL experts around the world and is available in more than a dozen languages. It is Free Software released under the <a href=\"%s\">PostgreSQL License</a>."), "licence.php") . '</p>
 
 <div class="pgaBanner">
-' . sprintf(_("The current version of pgAdmin is <a href=\"/download/\">%s</a>."), "1.12.0") . '
+' . sprintf(_("The current version of pgAdmin is <a href=\"/download/\">%s</a>."), "1.12.1") . '
 </div>
 
 '; 

1323b2e7477b9a1955276d8bd518dc2d01dd5ee3
Guillaume Lelarge <guillaume@lelarge.info>
2011-07-02 16:54:46 +0200

    New roadmap.

--- development/roadmap.php
+++ development/roadmap.php
@@ -7,14 +7,13 @@ $PAGE_CONTENT = '
 
 <h1>' . _("Roadmap") . '</h1>
 
-<p>' . sprintf(_("This roadmap outlines the work that is hoped will be done in the %s development cycle for release in version %s of pgAdmin."), "1.13", "1.14") . '</p>
+<p>' . sprintf(_("This roadmap outlines the work that is hoped will be done in the %s development cycle for release in version %s of pgAdmin."), "1.15", "1.16") . '</p>
 
 <ul>
   <li>' . _("PostgreSQL compatibility") . '<br />&nbsp;
     <ul>
-      <li>' . _("Complete 9.1 support") . '<br />&nbsp;</li>
-      <li><del>' . _("Finish 9.0 support with exclusion constraints [GL]") .  '</del><br />&nbsp;</li>
-      <li>' . _("Finish 8.4 support with SQL/Med objects [GL]") . '<br />&nbsp;</li>
+      <li>' . _("Complete 9.2 support") . '<br />&nbsp;</li>
+      <li>' . _("Finish 9.0 support with security labels [GL]") .  '</del><br />&nbsp;</li>
       <li>' . _("Finish 8.3 support with operator family objets' creation [GL]") . '<br />&nbsp;</li>
       <li>' . _("Finish 7.4 (?) support with operator class objects' creation [GL]") . '<br />&nbsp;</li>
     </ul>
@@ -29,13 +28,10 @@ $PAGE_CONTENT = '
 
   <li>' . _("Functionality") . '<br />&nbsp;
     <ul>
-      <li><del>' . _("Better i18n support [GL]") . '</del><br />&nbsp;</li>
-      <li><del>' . _("Show/hide columns in frmStatus's reports [GL]") . '</del><br />&nbsp;</li>
-      <li>' . _("Filter rows in frmStatus's reports [GL]") . '<br />&nbsp;</li>
-      <li>' . _("Logfile: columns according to log_line_prefix, filename according to log_filename, etc. in frmStatus [GL]") . '<br />&nbsp;</li>
+      <li>' . _("New options dialog [GL]") . '<br />&nbsp;</li>
       <li>' . _("Custom reports in frmStatus [GL]") . '<br />&nbsp;</li>
       <li>' . _("Graphs in frmStatus [GL]") . '<br />&nbsp;</li>
-      <li>' . _("Redesign Table dialogue to remove sub-dialogues where possible and replace with grid style UI [DP]") . '<br />&nbsp;</li>
+      <li>' . _("Redesign Table dialogue to remove sub-dialogues where possible and replace with grid style UI [GL]") . '<br />&nbsp;</li>
       <li>' . _("Data tune-up options for slow queries (requires XML EXPLAIN in PG 8.4) [DP]") . '<br />&nbsp;</li>
       <li>' . _("pgsql-performance report tool for problematic queries (requires XML EXPLAIN in PG 8.4) [DP]") . '<br />&nbsp;</li>
       <li>' . _("Add a dialog to allow the user to set a global breakpoint in a specific backend process [DP]") . '<br />&nbsp;</li>

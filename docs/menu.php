b6cf38761b2ce8c5acd1db812ccc186f7e38c5ce
Dave Page <dpage@pgadmin.org>
2011-08-18 13:41:33 +0100

    Add links for the 1.14 docs.

--- docs/menu.php
+++ docs/menu.php
@@ -4,7 +4,8 @@ function www_menu()
 {
   $output = '
 <div' . www_current_menu_page("index.php") . '><a href="index.php">&rsaquo; ' . _("Introduction") . '</a></div>
-<div' . www_current_menu_page("dev/index.html") . '><a href="dev/index.html">&rsaquo; ' . sprintf(_("Dev "), "1.11") . '</a></div>
+<div' . www_current_menu_page("dev/index.html") . '><a href="dev/index.html">&rsaquo; ' . _("Dev") . '</a></div>
+<div' . www_current_menu_page("1.14/index.html") . '><a href="1.14/index.html">&rsaquo; ' . sprintf(_("Release %s"), "1.14") . '</a></div>
 <div' . www_current_menu_page("1.12/index.html") . '><a href="1.12/index.html">&rsaquo; ' . sprintf(_("Release %s"), "1.12") . '</a></div>
 <div' . www_current_menu_page("1.10/index.html") . '><a href="1.10/index.html">&rsaquo; ' . sprintf(_("Release %s"), "1.10") . '</a></div>
 <div' . www_current_menu_page("1.8/index.html") . '><a href="1.8/index.html">&rsaquo; ' . sprintf(_("Release %s"), "1.8") . '</a></div>

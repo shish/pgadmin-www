d28df6c3a4aa58e4ecad23e00990d89402cd0f8f
Magnus Hagander <magnus@hagander.net>
2012-06-28 13:26:40 +0200

    Remove pages for unmaintained packages
    Replace this with a short paragraph on the first page referencing the
    yum repository, debian backports and ubuntu ppa.

--- download/index.php
+++ download/index.php
@@ -10,6 +10,8 @@ $PAGE_CONTENT = '
 <p>' . sprintf(_("pgAdmin is a free software project released under the <a href=\"%s\">PostgreSQL/Artistic licence</a>. The software is available in source and binary format from the <a href=\"%s\">PostgreSQL mirror network</a>. Because compiling from source requires technical knowledge, we recommend installing binary packages whenever possible."), "../licence.php", "http://www.postgresql.org/ftp/pgadmin3/release/") . '</p>
 
 <p>' . _("The pages in this section give additional details about each binary package available as well as more direct download links. In addition, you can download source tarballs, additional language translations, pgAgent and admin packs for your servers to enable additional functionality.") . '</p>
+
+<p>' . sprintf(_("Finally, pgAdmin is included by default in most Linux distributions, such as RedHat, Fedora, Debian and Ubuntu. If you want a different version than the one included in the distribution, you can use channels such as the PostgreSQL <a href=\"%s\">yum repository</a>, Debian <a href=\"%s\">backports</a> or the Ubuntu <a href=\"%s\">PPA</a>."), "http://yum.postgresql.org/", "http://backports.debian.org/","https://launchpad.net/~pitti/+archive/postgresql") . '</p>
 '; 
 
 // Display the page
--- download/menu.php
+++ download/menu.php
@@ -4,15 +4,8 @@ function www_menu()
 {
   $output = '
 <div' . www_current_menu_page("index.php") . '><a href="index.php">&rsaquo; ' . _("Introduction") . '</a></div>
-<!-- <div' . www_current_menu_page("debian.php") . '><a href="debian.php">&rsaquo; ' . _("Debian") . '</a></div> -->
-<div' . www_current_menu_page("freebsd.php") . '><a href="freebsd.php">&rsaquo; ' . _("FreeBSD") . '</a></div>
 <div' . www_current_menu_page("macosx.php") . '><a href="macosx.php">&rsaquo; ' . _("Mac OSX") . '</a></div>
-<div' . www_current_menu_page("opensuse.php") . '><a href="opensuse.php">&rsaquo; ' . _("OpenSUSE") . '</a></div>
-<div' . www_current_menu_page("rpm.php") . '><a href="rpm.php">&rsaquo; ' . _("RPM") . '</a></div>
-<div' . www_current_menu_page("slackware.php") . '><a href="slackware.php">&rsaquo; ' . _("Slackware") . '</a></div>
-<div' . www_current_menu_page("solaris.php") . '><a href="solaris.php">&rsaquo; ' . _("Solaris") . '</a></div>
 <div' . www_current_menu_page("source.php") . '><a href="source.php">&rsaquo; ' . _("Source code") . '</a></div>
-<!-- <div' . www_current_menu_page("ubuntu.php") . '><a href="ubuntu.php">&rsaquo; ' . _("Ubuntu") . '</a></div> -->
 <div' . www_current_menu_page("windows.php") . '><a href="windows.php">&rsaquo; ' . _("Windows &trade;") . '</a></div>
 <br />
 <div' . www_current_menu_page("adminpacks.php") . '><a href="adminpacks.php">&rsaquo; ' . _("Admin packs") . '</a></div>

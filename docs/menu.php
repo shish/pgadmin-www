e917cd8189173d68f51329161d52177c00abf46a
Dave Page <dpage@pgadmin.org>
2012-07-11 19:47:51 +0100

    Update website with the 1.16 docs.

--- docs/menu.php
+++ docs/menu.php
@@ -5,6 +5,7 @@ function www_menu()
   $output = '
 <div' . www_current_menu_page("index.php") . '><a href="index.php">&rsaquo; ' . _("Introduction") . '</a></div>
 <div' . www_current_menu_page("dev/index.html") . '><a href="dev/index.html">&rsaquo; ' . _("Dev") . '</a></div>
+<div' . www_current_menu_page("1.16/index.html") . '><a href="1.16/index.html">&rsaquo; ' . sprintf(_("Release %s"), "1.16") . '</a></div>
 <div' . www_current_menu_page("1.14/index.html") . '><a href="1.14/index.html">&rsaquo; ' . sprintf(_("Release %s"), "1.14") . '</a></div>
 <div' . www_current_menu_page("1.12/index.html") . '><a href="1.12/index.html">&rsaquo; ' . sprintf(_("Release %s"), "1.12") . '</a></div>
 <div' . www_current_menu_page("1.10/index.html") . '><a href="1.10/index.html">&rsaquo; ' . sprintf(_("Release %s"), "1.10") . '</a></div>
--- include/doc.php
+++ include/doc.php
@@ -45,6 +45,12 @@ if (isset($_GET['docset']))
       $docheader = sprintf(_("pgAdmin %s online documentation"), "1.14");
       break;
 
+    case "1.16":
+      $docroot_lang = $_SERVER['DOCUMENT_ROOT'] . "/svnrepo/pgadmin3-1.16/docs/".$_SESSION['xPGA']['lang']."/_build/html";
+      $docroot = $_SERVER['DOCUMENT_ROOT'] . "/svnrepo/pgadmin3-1.16/docs/en_US/_build/html";
+      $docheader = sprintf(_("pgAdmin %s online documentation"), _("1.16"));
+      break;
+
     case "dev":
       $docroot_lang = $_SERVER['DOCUMENT_ROOT'] . "/svnrepo/pgadmin3/docs/".$_SESSION['xPGA']['lang']."/_build/html";
       $docroot = $_SERVER['DOCUMENT_ROOT'] . "/svnrepo/pgadmin3/docs/en_US/_build/html";

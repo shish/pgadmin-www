dab50c1d5458439dd7aaf85acbacd0eb22e3b7e2
Shish <shish@shishnet.org>
2013-09-05 13:53:30 +0100

    Suse 10 and XGL are no longer hip with the kids...

--- screenshots/menu.php
+++ screenshots/menu.php
@@ -9,7 +9,7 @@ function www_menu()
 <div' . www_current_menu_page("documentation.php") . '><a href="documentation.php">&rsaquo; ' . _("Documentation") . '</a></div>
 <div' . www_current_menu_page("mb_i18n.php") . '><a href="mb_i18n.php">&rsaquo; ' . _("Multibyte &amp; i18n") . '</a></div>
 <div' . www_current_menu_page("serverstatus.php") . '><a href="serverstatus.php">&rsaquo; ' . _("Server status") . '</a></div>
-<div' . www_current_menu_page("xgl.php") . '><a href="xgl.php">&rsaquo; ' . _("Suse 10 XGL") . '</a></div>
+<!-- <div' . www_current_menu_page("xgl.php") . '><a href="xgl.php">&rsaquo; ' . _("Suse 10 XGL") . '</a></div> -->
 ';
 
   return $output;
